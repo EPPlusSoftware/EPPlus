583d08f16b0d34e4c099deddb64d10fa4e9b83ae
Jan Källman <jan.kallman@epplussoftware.com>
2020-01-31 11:15:56 +0100

    Added basic test for OfficeProperties

---
+++ src/EPPlusTest/OfficePropertiesTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPPlusTest
+{
+    [TestClass]
+    public class OfficePropertiesTests
+    {
+        
+        [TestMethod]
+        public void ValidateLong()
+        {
+            using (var pck=new ExcelPackage())
+            {
+                var ticks = DateTime.Now.Ticks;
+                pck.Workbook.Properties.SetCustomPropertyValue("Timestamp", ticks);
+                pck.Workbook.Worksheets.Add("Test");
+
+                pck.Save();
+
+                using(var pck2=new ExcelPackage(pck.Stream))
+                {
+                    Assert.AreEqual((double)ticks, pck.Workbook.Properties.GetCustomPropertyValue("Timestamp"));
+                }
+            }
+        }
+    }
+}
