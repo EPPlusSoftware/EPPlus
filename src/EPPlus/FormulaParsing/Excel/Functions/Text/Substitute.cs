de91abc29025164ccb5d4fa956f03387d460ccfc
Taneli Waltari <taneli_waltari@hotmail.com>
2020-02-04 15:56:13 +0200

    Support Instance_num parameter of SUBSTITUTE function. Fixed EPPPlus 4.5 issue #530. see https://github.com/JanKallman/EPPlus/pull/531/files.

--- src/EPPlus/FormulaParsing/Excel/Functions/Text/Substitute.cs
+++ src/EPPlus/FormulaParsing/Excel/Functions/Text/Substitute.cs
@@ -26,8 +26,20 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Text
             var text = ArgToString(arguments, 0);
             var find = ArgToString(arguments, 1);
             var replaceWith = ArgToString(arguments, 2);
-            var result = text.Replace(find, replaceWith);
+            var result = arguments.Count() > 3 ? ReplaceFirst(text, find, replaceWith, ArgToInt(arguments, 3)) : text.Replace(find, replaceWith);
             return CreateResult(result, DataType.String);
+        }
+
+        private static string ReplaceFirst(string text, string search, string replace, int instanceNumber)
+        {
+            int pos = -1;
+            for (int i = 0; i < instanceNumber; i++)
+            {
+                pos = text.IndexOf(search, pos + 1);
+                if (pos < 0)
+                    break;
+            }
+            return pos < 0 ? text : text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
         }
     }
 }
