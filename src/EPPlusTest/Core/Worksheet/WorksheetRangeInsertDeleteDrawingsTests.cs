f5e4c217229189a80fe2846c922f5b8b2daab6f2
Jan Källman <jan.kallman@epplussoftware.com>
2020-02-17 07:40:12 +0100

    Added some more tests

---
+++ src/EPPlusTest/Core/Worksheet/WorksheetRangeInsertDeleteDrawingsTests.cs
@@ -0,0 +1,271 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPPlusTest.Core.Worksheet
+{
+    [TestClass]
+    public class WorksheetRangeInsertDeleteTests : TestBase
+    {
+        public static ExcelPackage _pck;
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            InitBase();
+            _pck = OpenPackage("WorksheetRangeInsertDeleteDrawings.xlsx", true);
+        }
+        [ClassCleanup] 
+        public static void Cleanup()
+        {
+            SaveAndCleanup(_pck);
+        }
+        #region Row Tests
+        [TestMethod]
+        public void InsertRowWithDrawing()
+        {
+            //Setup
+            var ws = _pck.Workbook.Worksheets.Add("DrawingsInsertRow");
+            var shape = ws.Drawings.AddShape("Shape1_TwoCell",OfficeOpenXml.Drawing.eShapeStyle.Rect);
+
+            var pic =  ws.Drawings.AddPicture("Picture1_OneCell", Properties.Resources.Test1);
+            pic.SetPosition(0, 0, 11, 0);
+
+            var chart = ws.Drawings.AddLineChart("Chart1_TwoCellAbsolute", OfficeOpenXml.Drawing.Chart.eLineChartType.Line);
+            chart.SetPosition(0, 0, 22, 0);
+            chart.EditAs = OfficeOpenXml.Drawing.eEditAs.Absolute;
+
+            int picToRow = pic.To.Row;
+
+            //Act
+            ws.InsertRow(1, 1);
+            ws.InsertRow(3, 1);
+
+            //Assert
+            Assert.AreEqual(1, shape.From.Row);
+            Assert.AreEqual(1, pic.From.Row);
+            Assert.AreEqual(0, chart.From.Row);
+
+            Assert.AreEqual(12, shape.To.Row);
+            Assert.AreEqual(picToRow+1, pic.To.Row);
+            Assert.AreEqual(10, chart.To.Row);
+        }
+        [TestMethod]
+        public void DeleteRowWithDrawing()
+        {
+            //Setup
+            var ws = _pck.Workbook.Worksheets.Add("DrawingsDeleteRow");
+            var shape = ws.Drawings.AddShape("Shape1_TwoCell", OfficeOpenXml.Drawing.eShapeStyle.Rect);
+            shape.SetPosition(1, 0, 0, 0);
+
+            var pic = ws.Drawings.AddPicture("Picture1_OneCell", Properties.Resources.Test1);
+            pic.SetPosition(1, 0, 11, 0);
+
+            var chart = ws.Drawings.AddLineChart("Chart1_TwoCellAbsolute", OfficeOpenXml.Drawing.Chart.eLineChartType.Line);
+            chart.SetPosition(1, 0, 22, 0);
+            chart.EditAs = OfficeOpenXml.Drawing.eEditAs.Absolute;
+
+            int picToRow = pic.To.Row;
+
+            //Act
+            ws.DeleteRow(1, 1);
+            ws.DeleteRow(3, 1);
+
+            //Assert
+            Assert.AreEqual(0, shape.From.Row);
+            Assert.AreEqual(0, pic.From.Row);
+            Assert.AreEqual(1, chart.From.Row);
+
+            Assert.AreEqual(9, shape.To.Row);
+            Assert.AreEqual(picToRow - 1, pic.To.Row);
+            Assert.AreEqual(11, chart.To.Row);
+        }
+        [TestMethod]
+        public void DeleteRowsEntireDrawing()
+        {
+            //Setup
+            var ws = _pck.Workbook.Worksheets.Add("DrawingsDeleteEntireRow");
+            var shape1 = ws.Drawings.AddShape("Shape1", OfficeOpenXml.Drawing.eShapeStyle.Rect);
+            
+            var shape2 = ws.Drawings.AddShape("DeletedShape", OfficeOpenXml.Drawing.eShapeStyle.Rect);
+            shape2.SetPosition(2, 0, 11, 0);
+
+            var shape3 = ws.Drawings.AddShape("Shape3", OfficeOpenXml.Drawing.eShapeStyle.Rect);
+            shape3.SetPosition(5, 0, 22, 0);
+
+            //Act
+            ws.DeleteRow(3, 10);  
+
+            //Assert
+            Assert.AreEqual(2, ws.Drawings.Count);
+
+            Assert.AreEqual(0, shape1.From.Row);
+            Assert.AreEqual(2, shape1.To.Row);
+
+            Assert.AreEqual(2, shape3.From.Row);
+            Assert.AreEqual(5, shape3.To.Row);
+        }
+        [TestMethod]
+        public void DeleteRowsDrawingPartialRow()
+        {
+            //Setup
+            var ws = _pck.Workbook.Worksheets.Add("DrawingsDeletePartialRow");
+            var shape1 = ws.Drawings.AddShape("Shape1", OfficeOpenXml.Drawing.eShapeStyle.Rect);
+            shape1.SetPosition(0, 5, 0, 0);
+
+            var shape2 = ws.Drawings.AddShape("PartialShape", OfficeOpenXml.Drawing.eShapeStyle.Rect);
+            shape2.SetPosition(2, 5, 11, 0);
+
+            var shape3 = ws.Drawings.AddShape("Shape3", OfficeOpenXml.Drawing.eShapeStyle.Rect);
+            shape3.SetPosition(5, 5, 22, 0);
+
+            //Act
+            ws.DeleteRow(3, 10);
+
+            //Assert
+            Assert.AreEqual(3, ws.Drawings.Count);
+
+            Assert.AreEqual(0, shape1.From.Row);
+            Assert.AreEqual(5, shape1.From.RowOff / ExcelDrawing.EMU_PER_PIXEL);
+            Assert.AreEqual(2, shape1.To.Row);
+            Assert.AreEqual(0, shape1.To.RowOff);
+
+            Assert.AreEqual(2, shape2.From.Row);
+            Assert.AreEqual(0, shape2.From.RowOff);
+            Assert.AreEqual(2, shape2.To.Row);
+            Assert.AreEqual(5, shape2.To.RowOff/ExcelDrawing.EMU_PER_PIXEL);
+
+            Assert.AreEqual(2, shape3.From.Row);
+            Assert.AreEqual(0, shape3.From.RowOff);
+            Assert.AreEqual(5, shape3.To.Row);
+            Assert.AreEqual(5, shape3.To.RowOff / ExcelDrawing.EMU_PER_PIXEL);
+        }
+        #endregion
+        #region Column Tests
+        [TestMethod]
+        public void InsertColumnWithDrawing()
+        {
+            //Setup
+            var ws = _pck.Workbook.Worksheets.Add("DrawingsInsertColumn");
+            var shape = ws.Drawings.AddShape("Shape1_TwoCell", OfficeOpenXml.Drawing.eShapeStyle.Rect);
+
+            var pic = ws.Drawings.AddPicture("Picture1_OneCell", Properties.Resources.Test1);
+            pic.SetPosition(20, 0, 0, 0);
+
+            var chart = ws.Drawings.AddLineChart("Chart1_TwoCellAbsolute", OfficeOpenXml.Drawing.Chart.eLineChartType.Line);
+            chart.SetPosition(40, 0, 0, 0);
+            chart.EditAs = OfficeOpenXml.Drawing.eEditAs.Absolute;
+
+            int picToCol = pic.To.Column;
+
+            //Act
+            ws.InsertColumn(1, 1);
+            ws.InsertColumn(3, 1);
+
+            //Assert
+            Assert.AreEqual(1, shape.From.Column);
+            Assert.AreEqual(1, pic.From.Column);
+            Assert.AreEqual(0, chart.From.Column);
+
+            Assert.AreEqual(12, shape.To.Column);
+            Assert.AreEqual(picToCol + 1, pic.To.Column);
+            Assert.AreEqual(10, chart.To.Column);
+        }
+        [TestMethod]
+        public void DeleteColumnWithDrawing()
+        {
+            //Setup
+            var ws = _pck.Workbook.Worksheets.Add("DrawingsDeleteColumns");
+            var shape = ws.Drawings.AddShape("Shape1_TwoCell", eShapeStyle.Rect);
+            shape.SetPosition(0, 0, 1, 0);
+
+            var pic = ws.Drawings.AddPicture("Picture1_OneCell", Properties.Resources.Test1);
+            pic.SetPosition(11, 0, 1, 0);
+
+            var chart = ws.Drawings.AddLineChart("Chart1_TwoCellAbsolute", OfficeOpenXml.Drawing.Chart.eLineChartType.Line);
+            chart.SetPosition(22, 0, 1, 0);
+            chart.EditAs = OfficeOpenXml.Drawing.eEditAs.Absolute;
+
+            int picToColumn = pic.To.Column;
+
+            //Act
+            ws.DeleteColumn(1, 1);
+            ws.DeleteColumn(3, 1);
+
+            //Assert
+            Assert.AreEqual(0, shape.From.Column);
+            Assert.AreEqual(0, pic.From.Column);
+            Assert.AreEqual(1, chart.From.Column);
+
+            Assert.AreEqual(9, shape.To.Column);
+            Assert.AreEqual(picToColumn - 1, pic.To.Column);
+            Assert.AreEqual(11, chart.To.Column);
+        }
+        [TestMethod]
+        public void DeleteColumnEntireDrawing()
+        {
+            //Setup
+            var ws = _pck.Workbook.Worksheets.Add("DrawingsDeleteEntireColumn");
+            var shape1 = ws.Drawings.AddShape("Shape1", OfficeOpenXml.Drawing.eShapeStyle.Rect);
+
+            var shape2 = ws.Drawings.AddShape("DeletedShape", OfficeOpenXml.Drawing.eShapeStyle.Rect);
+            shape2.SetPosition(11, 0, 2, 0);
+
+            var shape3 = ws.Drawings.AddShape("Shape3", OfficeOpenXml.Drawing.eShapeStyle.Rect);
+            shape3.SetPosition(22, 0, 5, 0);
+
+            //Act
+            ws.DeleteColumn(3, 10);
+
+            //Assert
+            Assert.AreEqual(2, ws.Drawings.Count);
+
+            Assert.AreEqual(0, shape1.From.Column);
+            Assert.AreEqual(2, shape1.To.Column);
+
+            Assert.AreEqual(2, shape3.From.Column);
+            Assert.AreEqual(5, shape3.To.Column);
+        }
+        [TestMethod]
+        public void DeleteColumnDrawingPartialColumn()
+        {
+            //Setup
+            var ws = _pck.Workbook.Worksheets.Add("DrawingsDeletePartialColumn");
+            var shape1 = ws.Drawings.AddShape("Shape1", eShapeStyle.Rect);
+            shape1.SetPosition(0, 0, 0, 5);
+
+            var shape2 = ws.Drawings.AddShape("PartialShape", eShapeStyle.Rect);
+            shape2.SetPosition(11, 0, 2, 5);
+
+            var shape3 = ws.Drawings.AddShape("Shape3", eShapeStyle.Rect);
+            shape3.SetPosition(22, 0, 5, 5);
+
+            //Act
+            ws.DeleteColumn(3, 10);
+
+            //Assert
+            Assert.AreEqual(3, ws.Drawings.Count);
+
+            Assert.AreEqual(0, shape1.From.Column);
+            Assert.AreEqual(5, shape1.From.ColumnOff / ExcelDrawing.EMU_PER_PIXEL);
+            Assert.AreEqual(2, shape1.To.Column);
+            Assert.AreEqual(0, shape1.To.ColumnOff);
+
+            Assert.AreEqual(2, shape2.From.Column);
+            Assert.AreEqual(0, shape2.From.ColumnOff);
+            Assert.AreEqual(2, shape2.To.Column);
+            Assert.AreEqual(5, shape2.To.ColumnOff / ExcelDrawing.EMU_PER_PIXEL);
+
+            Assert.AreEqual(2, shape3.From.Column);
+            Assert.AreEqual(0, shape3.From.ColumnOff);
+            Assert.AreEqual(5, shape3.To.Column);
+            Assert.AreEqual(5, shape3.To.ColumnOff / ExcelDrawing.EMU_PER_PIXEL);
+        }
+
+        #endregion
+    }
+}
---
+++ src/EPPlusTest/Core/Worksheet/WorksheetRangeInsertDeleteTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPPlusTest.Core.Worksheet
+{
+    [TestClass]
+    public class WorksheetRangeInsertTests : TestBase
+    {
+        public static ExcelPackage _pck;
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            InitBase();
+            _pck = OpenPackage("WorksheetRangeInsert.xlsx", true);
+        }
+        [ClassCleanup] 
+        public static void Cleanup()
+        {
+            SaveAndCleanup(_pck);
+        }
+        [TestMethod]
+        public void ValidateFormulasAfterInsertRow()
+        {
+            //Setup
+            var ws = _pck.Workbook.Worksheets.Add("Sheet1");
+            var ws2 = _pck.Workbook.Worksheets.Add("Sheet2");
+            ws.Cells["A1"].Formula="Sum(C5:C10)";
+            ws.Cells["B1:B2"].Formula = "Sum(C5:C10)";
+            ws2.Cells["A1"].Formula = "Sheet2!Sum(C5:C10)";
+            ws2.Cells["B1:B2"].Formula = "Sheet2!Sum(C5:C10)";
+
+            //Act
+            ws.InsertRow(3, 1);
+
+            //Assert
+            Assert.AreEqual("Sum(C6:C11)", ws.Cells["A1"].Formula);
+            Assert.AreEqual("Sum(C6:C11)", ws.Cells["B1"].Formula);
+            Assert.AreEqual("Sum(C7:C12)", ws.Cells["B2"].Formula);
+        }
+        [TestMethod]
+        public void ValidateFormulasAfterDeleteRow()
+        {
+            //Setup
+            var ws = _pck.Workbook.Worksheets.Add("Sheet1");
+            var ws2 = _pck.Workbook.Worksheets.Add("Sheet2");
+            ws.Cells["A1"].Formula = "Sum(C5:C10)";
+            ws.Cells["B1:B2"].Formula = "Sum(C5:C10)";
+            ws2.Cells["A1"].Formula = "Sheet2!Sum(C5:C10)";
+            ws2.Cells["B1:B2"].Formula = "Sheet2!Sum(C5:C10)";
+
+            //Act
+            ws.DeleteRow(3, 1);
+
+            //Assert
+            Assert.AreEqual("Sum(C4:C9)", ws.Cells["A1"].Formula);
+            Assert.AreEqual("Sum(C4:C9)", ws.Cells["B1"].Formula);
+            Assert.AreEqual("Sum(C5:C10)", ws.Cells["B2"].Formula);
+        }
+        [TestMethod]
+        public void ValidateFormulasAfterDelete2Rows()
+        {
+            //Setup
+            var ws = _pck.Workbook.Worksheets.Add("Sheet1");
+            ws.Cells["B3:B6"].Formula = "A1";
+            //Act
+            ws.DeleteRow(2, 2);
+
+            //Assert
+            Assert.AreEqual("",ws.Cells["B1"].Formula);
+            Assert.AreEqual("#REF!", ws.Cells["B2"].Formula);
+            Assert.AreEqual("#REF!", ws.Cells["B3"].Formula);
+            Assert.AreEqual("A2", ws.Cells["B4"].Formula);
+            Assert.IsNull(ws.Cells["B6"].Formula);
+        }
+    }
+}
