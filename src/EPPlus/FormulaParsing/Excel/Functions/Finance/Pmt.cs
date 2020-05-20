73e9433673bdd5db675322ad5179a530ccb2064a
Mats Alm <897655+swmal@users.noreply.github.com>
2020-05-20 06:50:29 +0200

    Added a test for PMT/beginning of period, changed PMT to ported implementation

--- src/EPPlus/FormulaParsing/Excel/Functions/Finance/Pmt.cs
+++ src/EPPlus/FormulaParsing/Excel/Functions/Finance/Pmt.cs
@@ -14,6 +14,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Finance.Implementations;
 using OfficeOpenXml.FormulaParsing.ExpressionGraph;
 
 namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Finance
@@ -26,17 +27,14 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Finance
             var rate = ArgToDecimal(arguments, 0);
             var nPer = ArgToInt(arguments, 1);
             var presentValue = ArgToDecimal(arguments, 2);
-            var payEndOfPeriod = false;
+            var payEndOfPeriod = 0;
             var futureValue = 0d;
             if (arguments.Count() > 3) futureValue = ArgToDecimal(arguments, 3);
-            if (arguments.Count() > 4) payEndOfPeriod = ArgToBool(arguments, 4);
+            if (arguments.Count() > 4) payEndOfPeriod = ArgToInt(arguments, 4);
+            var result = InternalMethods.PMT_Internal(rate, nPer, presentValue, futureValue, payEndOfPeriod == 0 ? PmtDue.EndOfPeriod : PmtDue.BeginningOfPeriod);
+            if (result.HasError) return CreateResult(result.ExcelErrorType);
 
-            var result = (futureValue + presentValue * System.Math.Pow(rate + 1, nPer)) * rate
-                      /
-                   ((payEndOfPeriod ? rate + 1 : 1) * (1 - System.Math.Pow(rate + 1, nPer)));
-       
-
-            return CreateResult(result, DataType.Decimal);
+            return CreateResult(result.Result, DataType.Decimal);
         }
 
         private static double GetInterest(double rate, double remainingAmount)
--- src/EPPlusTest/FormulaParsing/Excel/Functions/FinanceFunctionTests.cs
+++ src/EPPlusTest/FormulaParsing/Excel/Functions/FinanceFunctionTests.cs
@@ -36,7 +36,7 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions
     public class FinanceFunctionTests
     {
         [TestMethod]
-        public void PmtTest1()
+        public void PmtTest_EndOfPeriod()
         {
             using (var package = new ExcelPackage())
             {
@@ -46,6 +46,20 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions
                 var value = sheet.Cells["A1"].Value;
                 var value2 = System.Math.Round(Convert.ToDouble(value), 2);
                 Assert.AreEqual(-943.56, value2);
+            }
+        }
+
+        [TestMethod]
+        public void PmtTest_BeginningOfPeriod()
+        {
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("test");
+                sheet.Cells["A1"].Formula = "PMT( 5%/12, 60, 50000, 0, 1 )";
+                sheet.Calculate();
+                var value = sheet.Cells["A1"].Value;
+                var value2 = System.Math.Round(Convert.ToDouble(value), 2);
+                Assert.AreEqual(-939.65, value2);
             }
         }
     }
