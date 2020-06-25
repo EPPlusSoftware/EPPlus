327546bd98610b65a29f3edf2d5cfef7c09c66c3
rdhasse <rdhasse@ieee.org>
2020-06-25 16:06:29 -0500

    Added 3 UTs, 2 exhibit the issue

--- src/EPPlusTest/FormulaParsing/Excel/Functions/RefAndLookup/IndexTests.cs
+++ src/EPPlusTest/FormulaParsing/Excel/Functions/RefAndLookup/IndexTests.cs
@@ -30,6 +30,7 @@ using System.IO;
 using EPPlusTest.FormulaParsing.TestHelpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
 using OfficeOpenXml.FormulaParsing;
 using OfficeOpenXml.FormulaParsing.Excel.Functions.RefAndLookup;
 
@@ -80,6 +81,51 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.RefAndLookup
             _worksheet.Calculate();
 
             Assert.AreEqual(5d, _worksheet.Cells["A4"].Value);
+        }
+
+        [TestMethod]
+        public void Match_Without_Wildcard()
+        {
+            _worksheet.Cells["A1"].Value = "test";
+            _worksheet.Cells["A2"].Value = "value_to_match";
+            _worksheet.Cells["A3"].Value = "test";
+
+            //_worksheet.Cells["A4"].Value 
+            _worksheet.Cells["A4"].Formula = "MATCH(\"value_to_match\", A1:A3, 0)";
+
+            _worksheet.Calculate();
+
+            Assert.AreEqual(2, _worksheet.Cells["A4"].Value);
+        }
+
+        [TestMethod]
+        public void Match_With_Wildcard1()
+        {
+            _worksheet.Cells["A1"].Value = "test";
+            _worksheet.Cells["A2"].Value = "value_to_match";
+            _worksheet.Cells["A3"].Value = "test";
+
+            //_worksheet.Cells["A4"].Value 
+            _worksheet.Cells["A4"].Formula = "MATCH(\"*alue_to_match\", A1:A3, 0)";
+
+            _worksheet.Calculate();
+
+            Assert.AreEqual(2, _worksheet.Cells["A4"].Value);
+        }
+
+        [TestMethod]
+        public void Match_With_Wildcard2()
+        {
+            _worksheet.Cells["A1"].Value = "test";
+            _worksheet.Cells["A2"].Value = "value_to_match";
+            _worksheet.Cells["A3"].Value = "test";
+
+            //_worksheet.Cells["A4"].Value 
+            _worksheet.Cells["A4"].Formula = "MATCH(\"?alue_to_match\", A1:A3, 0)";
+
+            _worksheet.Calculate();
+
+            Assert.AreEqual(2, _worksheet.Cells["A4"].Value);
         }
     }
 }
