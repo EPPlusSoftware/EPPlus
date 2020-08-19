78ba62c18b2470c905203e0c6a52a6502897619a
Mats Alm <897655+swmal@users.noreply.github.com>
2020-08-19 23:31:42 +0200

    Fixed some invalid descriptions on IfError, IfNa and MaxIfs

--- src/EPPlus/FormulaParsing/Excel/Functions/Logical/MaxIfs.cs
+++ src/EPPlus/FormulaParsing/Excel/Functions/Logical/MaxIfs.cs
@@ -23,7 +23,7 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Logical
     [FunctionMetadata(
         Category = ExcelFunctionCategory.Logical,
         EPPlusVersion = "5.3",
-        Description = "Tests a user-defined condition and returns one result if the condition is TRUE, and another result if the condition is FALSE",
+        Description = "Returns the largest numeric value that meets one or more criteria in a range of values.",
         IntroducedInExcelVersion = "2019")]
     internal class MaxIfs : IfsWithMultipleMatchesBase
     {
