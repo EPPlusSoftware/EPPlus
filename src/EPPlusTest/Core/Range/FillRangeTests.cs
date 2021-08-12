9184bacb073494bb0d751faf1607e0048f5decad
Jan Källman <jan.kallman@epplussoftware.com>
2021-08-12 14:18:42 +0200

    Tests for FillDates

--- src/EPPlusTest/Core/Range/FillRangeTests.cs
+++ src/EPPlusTest/Core/Range/FillRangeTests.cs
@@ -8,12 +8,14 @@ namespace EPPlusTest.Core.Range
     public class RangeFillTests : TestBase
     {
         static ExcelPackage _pck;
-        static ExcelWorksheet _ws;
+        static ExcelWorksheet _wsNum;
+        static ExcelWorksheet _wsDate;
         [ClassInitialize]
         public static void Init(TestContext context)
         {
             _pck = OpenPackage("Range_Fill.xlsx", true);
-            _ws = _pck.Workbook.Worksheets.Add("FillNumbers");
+            _wsNum = _pck.Workbook.Worksheets.Add("FillNumbers");
+            _wsDate = _pck.Workbook.Worksheets.Add("FillDates");
         }
         [ClassCleanup]
         public static void Cleanup()
@@ -24,57 +26,57 @@ namespace EPPlusTest.Core.Range
         [TestMethod]
         public void FillNumbers_WithStartAndStep()
         {
-            _ws.Cells["A1:B5"].FillNumbers(2, 3);
+            _wsNum.Cells["A1:B5"].FillNumbers(2, 3);
             //Assert
-            Assert.AreEqual(3D, _ws.Cells["A1"].Value);
-            Assert.AreEqual(5D, _ws.Cells["A2"].Value);
-            Assert.AreEqual(7D, _ws.Cells["A3"].Value);
-            Assert.AreEqual(9D, _ws.Cells["A4"].Value);
-            Assert.AreEqual(11D, _ws.Cells["A5"].Value);
+            Assert.AreEqual(3D, _wsNum.Cells["A1"].Value);
+            Assert.AreEqual(5D, _wsNum.Cells["A2"].Value);
+            Assert.AreEqual(7D, _wsNum.Cells["A3"].Value);
+            Assert.AreEqual(9D, _wsNum.Cells["A4"].Value);
+            Assert.AreEqual(11D, _wsNum.Cells["A5"].Value);
 
-            Assert.AreEqual(3D, _ws.Cells["B1"].Value);
-            Assert.AreEqual(11D, _ws.Cells["B5"].Value);
+            Assert.AreEqual(3D, _wsNum.Cells["B1"].Value);
+            Assert.AreEqual(11D, _wsNum.Cells["B5"].Value);
         }
         [TestMethod]
-        public void FillNumbers_WithStep()
+        public void FillNumbers()
         {
-            _ws.Cells["C1"].Value = 7D;
-            _ws.Cells["D1"].Value = "D1";
-            _ws.Cells["D2"].Value = "D2";
-            _ws.Cells["C1:D5"].FillNumbers(1);
+            _wsNum.Cells["C1"].Value = 7D;
+            _wsNum.Cells["D1"].Value = "D1";
+            _wsNum.Cells["D2"].Value = "D2";
+            _wsNum.Cells["C1:D5"].FillNumbers();
             //Assert
-            Assert.AreEqual(7D, _ws.Cells["C1"].Value);
-            Assert.AreEqual(8D, _ws.Cells["C2"].Value);
-            Assert.AreEqual(9D, _ws.Cells["C3"].Value);
-            Assert.AreEqual(10D, _ws.Cells["C4"].Value);
-            Assert.AreEqual(11D, _ws.Cells["C5"].Value);
+            Assert.AreEqual(7D, _wsNum.Cells["C1"].Value);
+            Assert.AreEqual(8D, _wsNum.Cells["C2"].Value);
+            Assert.AreEqual(9D, _wsNum.Cells["C3"].Value);
+            Assert.AreEqual(10D, _wsNum.Cells["C4"].Value);
+            Assert.AreEqual(11D, _wsNum.Cells["C5"].Value);
 
-            Assert.AreEqual("D1", _ws.Cells["D1"].Value);
-            Assert.IsNull(_ws.Cells["D2"].Value);
+            Assert.AreEqual("D1", _wsNum.Cells["D1"].Value);
+            Assert.IsNull(_wsNum.Cells["D2"].Value);
         }
         [TestMethod]
         public void FillNumbers_RowWithStartAndStep()
         {
-            _ws.Cells["A10:E11"].FillNumbers(x =>
+            _wsNum.Cells["A10:E11"].FillNumbers(x =>
             {
                 x.Direction = eFillDirection.Row;
                 x.StartValue = 3;
                 x.StepValue = 2;
             }); 
             //Assert
-            Assert.AreEqual(3D, _ws.Cells["A10"].Value);
-            Assert.AreEqual(5D, _ws.Cells["B10"].Value);
-            Assert.AreEqual(7D, _ws.Cells["C10"].Value);
-            Assert.AreEqual(9D, _ws.Cells["D10"].Value);
-            Assert.AreEqual(11D, _ws.Cells["E10"].Value);
+            Assert.AreEqual(3D, _wsNum.Cells["A10"].Value);
+            Assert.AreEqual(5D, _wsNum.Cells["B10"].Value);
+            Assert.AreEqual(7D, _wsNum.Cells["C10"].Value);
+            Assert.AreEqual(9D, _wsNum.Cells["D10"].Value);
+            Assert.AreEqual(11D, _wsNum.Cells["E10"].Value);
 
-            Assert.AreEqual(3D, _ws.Cells["A11"].Value);
-            Assert.AreEqual(11D, _ws.Cells["E11"].Value);
+            Assert.AreEqual(3D, _wsNum.Cells["A11"].Value);
+            Assert.AreEqual(11D, _wsNum.Cells["E11"].Value);
         }
         [TestMethod]
         public void FillNumbers_RowWithStartAndStepMultipy()
         {
-            _ws.Cells["A13:E14"].FillNumbers(x =>
+            _wsNum.Cells["A13:E14"].FillNumbers(x =>
             {
                 x.CalculationMethod = eCalculationMethod.Multiply;
                 x.Direction = eFillDirection.Row;
@@ -82,29 +84,47 @@ namespace EPPlusTest.Core.Range
                 x.StepValue = 2;
             });
             //Assert
-            Assert.AreEqual(3D, _ws.Cells["A10"].Value);
-            Assert.AreEqual(6D, _ws.Cells["B10"].Value);
-            Assert.AreEqual(12D, _ws.Cells["C10"].Value);
-            Assert.AreEqual(24D, _ws.Cells["D10"].Value);
-            Assert.AreEqual(48D, _ws.Cells["E10"].Value);
+            Assert.AreEqual(3D, _wsNum.Cells["A10"].Value);
+            Assert.AreEqual(6D, _wsNum.Cells["B10"].Value);
+            Assert.AreEqual(12D, _wsNum.Cells["C10"].Value);
+            Assert.AreEqual(24D, _wsNum.Cells["D10"].Value);
+            Assert.AreEqual(48D, _wsNum.Cells["E10"].Value);
 
-            Assert.AreEqual(3D, _ws.Cells["A11"].Value);
-            Assert.AreEqual(48D, _ws.Cells["E11"].Value);
+            Assert.AreEqual(3D, _wsNum.Cells["A11"].Value);
+            Assert.AreEqual(48D, _wsNum.Cells["E11"].Value);
         }
         [TestMethod]
-        public void FillDate_WithStartAndStep()
+        public void FillDate_WithStartAndStepDay()
         {
             var startDate = new DateTime(2021, 1, 1);
-            _ws.Cells["A1:B5"].FillDates(new DateTime(2021,1,1));
+            _wsNum.Cells["A1:B5"].FillDates(new DateTime(2021,1,1));
             //Assert
-            Assert.AreEqual(startDate.Ticks, ((DateTime)_ws.Cells["A1"].Value).Ticks);
-            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay, ((DateTime)_ws.Cells["A2"].Value).Ticks);
-            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay*2, ((DateTime)_ws.Cells["A3"].Value).Ticks);
-            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay*3, ((DateTime)_ws.Cells["A4"].Value).Ticks);
-            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay*4, ((DateTime)_ws.Cells["A5"].Value).Ticks);
+            Assert.AreEqual(startDate.Ticks, ((DateTime)_wsNum.Cells["A1"].Value).Ticks);
+            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay, ((DateTime)_wsNum.Cells["A2"].Value).Ticks);
+            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay*2, ((DateTime)_wsNum.Cells["A3"].Value).Ticks);
+            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay*3, ((DateTime)_wsNum.Cells["A4"].Value).Ticks);
+            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay*4, ((DateTime)_wsNum.Cells["A5"].Value).Ticks);
 
-            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay, ((DateTime)_ws.Cells["B2"].Value).Ticks);
-            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay * 4, ((DateTime)_ws.Cells["B5"].Value).Ticks);
+            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay, ((DateTime)_wsNum.Cells["B2"].Value).Ticks);
+            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay * 4, ((DateTime)_wsNum.Cells["B5"].Value).Ticks);
         }
+        [TestMethod]
+        public void FillDate()
+        {
+            var startDate = new DateTime(2021, 6, 30);
+            _wsDate.Cells["C1"].Value = startDate;
+            _wsDate.Cells["D1"].Value = "D1";
+            _wsDate.Cells["C1:D5"].FillDates();
+            //Assert
+            Assert.AreEqual(startDate.Ticks, ((DateTime)_wsDate.Cells["C1"].Value).Ticks);
+            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay, ((DateTime)_wsDate.Cells["C2"].Value).Ticks);
+            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay * 2, ((DateTime)_wsDate.Cells["C3"].Value).Ticks);
+            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay * 3, ((DateTime)_wsDate.Cells["C4"].Value).Ticks);
+            Assert.AreEqual(startDate.Ticks + TimeSpan.TicksPerDay * 4, ((DateTime)_wsDate.Cells["C5"].Value).Ticks);
+
+            Assert.AreEqual("D1", _wsDate.Cells["D1"].Value);
+            Assert.IsNull(_wsDate.Cells["D2"].Value);
+        }
+
     }
 }
