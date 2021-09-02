cb7de6a0814b0b5154a2d4bc6e8f80f8c20e55a5
Jan Källman <jan.kallman@epplussoftware.com>
2021-09-02 15:47:29 +0200

    WIP:Added missing file

---
+++ src/EPPlus/Utils/EnumUtil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OfficeOpenXml.Utils
+{
+    internal static class EnumUtil
+    {
+        public static bool HasFlag<T>(T value, T flag) where T : Enum
+        {
+            return (Convert.ToInt32(value) & Convert.ToInt32(flag))==Convert.ToInt32(flag);
+        }
+    }
+}
