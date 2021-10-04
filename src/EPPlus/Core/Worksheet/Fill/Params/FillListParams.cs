68537616bc3c7bdd433c0232a46e7b88627ba313
Jan Källman <jan.kallman@epplussoftware.com>
2021-10-04 15:13:55 +0200

    Feature-ExcelRangeBase Fill methods (#503)
    * Started work on range fill. FillNumbers and FillDates methods
    
    * Tests for FillDates
    
    * More work and tests on FillNumber and FillDate
    
    * More test on Fill methods. Renamed FillNumber and FillDateTime
    
    * Added support for NumberFormat on Fills
    
    * Added method FillList
    
    * Added more tests for FillList
    
    * Added StartPosition to Fill methods params to enable fills from right to left and bottom to top
    
    * Xml docs. Renamed eDateUnit->eDateTimeUnit
    
    * eDateUnit->eDateTimeUnit

---
+++ src/EPPlus/Core/Worksheet/Fill/Params/FillListParams.cs
@@ -0,0 +1,26 @@
+﻿/*************************************************************************************************
+  Required Notice: Copyright (C) EPPlus Software AB. 
+  This software is licensed under PolyForm Noncommercial License 1.0.0 
+  and may only be used for noncommercial purposes 
+  https://polyformproject.org/licenses/noncommercial/1.0.0/
+
+  A commercial license to use this software can be purchased at https://epplussoftware.com
+ *************************************************************************************************
+  Date               Author                       Change
+ *************************************************************************************************
+    08/11/2021         EPPlus Software AB       EPPlus 5.8
+ *************************************************************************************************/
+namespace OfficeOpenXml.Core.Worksheet.Fill
+{
+    /// <summary>
+    /// Parameters for the <see cref="ExcelRangeBase.FillList{T}(System.Collections.Generic.IEnumerable{T}, System.Action{FillListParams})" /> method 
+    /// </summary>
+    public class FillListParams : FillParams
+    {
+        /// <summary>
+        /// The start index in the list. 
+        /// <seealso cref="FillParams.Direction"/>
+        /// </summary>
+        public int StartIndex { get; set; } = 0;
+    }
+}
---
+++ src/EPPlus/Core/Worksheet/Fill/Params/eFillStartPosition.cs
@@ -0,0 +1,30 @@
+﻿/*************************************************************************************************
+  Required Notice: Copyright (C) EPPlus Software AB. 
+  This software is licensed under PolyForm Noncommercial License 1.0.0 
+  and may only be used for noncommercial purposes 
+  https://polyformproject.org/licenses/noncommercial/1.0.0/
+
+  A commercial license to use this software can be purchased at https://epplussoftware.com
+ *************************************************************************************************
+  Date               Author                       Change
+ *************************************************************************************************
+    08/11/2021         EPPlus Software AB       EPPlus 5.8
+ *************************************************************************************************/
+namespace OfficeOpenXml
+{
+    /// <summary>
+    /// If the fill starts from the top-left cell or the bottom-right cell.
+    /// Also see <seealso cref="eFillDirection"/>
+    /// </summary>
+    public enum eFillStartPosition
+    {
+        /// <summary>
+        /// The fill starts from the top-left cell and fills to the left and down depending on the <see cref="eFillDirection"/>
+        /// </summary>
+        TopLeft,
+        /// <summary>
+        /// The fill starts from the bottom-right cell and fills to the right and up depending on the <see cref="eFillDirection"/>
+        /// </summary>
+        BottomRight
+    }
+}
