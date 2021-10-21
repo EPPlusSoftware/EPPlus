c9c7cc2455b305c2dee737d48387cff846b31c55
Mats Alm <897655+swmal@users.noreply.github.com>
2021-10-21 17:41:44 +0200

    #514 - fixed a bug where ExcelFunction.ArgToDecimal rethrow other error types as #VALUE

---
+++ src/EPPlusTest/FormulaParsing/ErrorCodesTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPPlusTest.FormulaParsing
+{
+    [TestClass]
+    public class ErrorCodesTests
+    {
+        [TestMethod]
+        public void ShouldSetDiv0InFunctions()
+        {
+            using(var package = new ExcelPackage())
+            {
+                var ws = package.Workbook.Worksheets.Add("test");
+                ws.Cells["A1"].Formula = "ROUND(2.3 + 1/0, 2)";
+                ws.Calculate();
+                Assert.AreEqual("#DIV/0!", ws.Cells["A1"].Value.ToString());
+            }
+        }
+    }
+}
