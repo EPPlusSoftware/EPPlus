87e72bfeedc1d9aa099885f656d49ea46018e7c7
Ossian Edström <ossian.edstrom@epplussoftware.com>
2023-02-14 09:26:54 +0100

    And imeMode

---
+++ src/EPPlus/DataValidation/ExcelDataValidationImeMode.cs
@@ -0,0 +1,17 @@
+﻿namespace OfficeOpenXml.DataValidation
+{
+    public enum ExcelDataValidationImeMode
+    {
+        noControl = 0,
+        off = 1,
+        on = 2,
+        disabled = 3,
+        hiragana = 4,
+        fullKatakana = 5,
+        halfKatakana = 6,
+        fullAlpha = 7,
+        halfAlpha = 8,
+        fullHangul = 9,
+        haldHangul = 10
+    }
+}
