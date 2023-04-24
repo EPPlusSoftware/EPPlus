b6ebf152edf90c67bd5d76955ede7ea20c48a40a
OssianEPPlus <122265629+OssianEPPlus@users.noreply.github.com>
2023-04-24 08:20:11 +0200

    Ensured Attribute encoding (#853)

---
+++ src/EPPlus/XMLWritingEncoder/AttributeEncoder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OfficeOpenXml.XMLWritingEncoder
+{
+    internal static class AttributeEncoder
+    {
+        /// <summary>
+        /// Encode to XML (special characteres: &apos; &quot; &gt; &lt; &amp;)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        internal static string EncodeXMLAttribute(this string s)
+        {
+            return s.Replace("&", "&amp;").Replace("<", "&lt;").Replace(">", "&gt;").Replace("\"", "&quot;").Replace("'", "&apos;");
+        }
+
+    }
+}
