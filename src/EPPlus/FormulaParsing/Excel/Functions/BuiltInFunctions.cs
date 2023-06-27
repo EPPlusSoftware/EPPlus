2a0355cdea5b4800d99d1ccd6784623fb2306d84
karlkallman <karl.kallman@epplussoftware.com>
2023-06-27 12:57:43 +0200

    Implemended IMSQRT function

--- src/EPPlus/FormulaParsing/Excel/Functions/BuiltInFunctions.cs
+++ src/EPPlus/FormulaParsing/Excel/Functions/BuiltInFunctions.cs
@@ -408,6 +408,8 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions
             Functions["complex"] = new Complex();
             Functions["imabs"] = new ImAbs();
             Functions["imexp"] = new ImExp();
+
+            Functions["imsqrt"] = new ImSqrt();
         }
     }
 }
