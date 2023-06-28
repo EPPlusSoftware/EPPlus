f7fc22eb5cafa7986146b4ec2e0fa361d4df87b8
karlkallman <karl.kallman@epplussoftware.com>
2023-06-28 14:05:27 +0200

    wip:IMTAN

--- src/EPPlusTest/FormulaParsing/Excel/Functions/Engineering/ImTanTests.cs
+++ src/EPPlusTest/FormulaParsing/Excel/Functions/Engineering/ImTanTests.cs
@@ -34,6 +34,63 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.Engineering
             Thread.CurrentThread.CurrentCulture = ci;
         }
 
+        [TestMethod]
+        public void ImTanShouldReturnCorrectiValue()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMTAN(\"-i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("-0.761594155955765i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImTanShouldReturnCorrectResultDecimalInput()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMTAN(\"22.46+34.567i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("1.52390779750415E-30+i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImTanShouldReturnCorrectiResult()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMTAN(\"200\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("-1.79252748379038", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+
+        }
     }
 
 }
