097db51e84596885f20ddda5a3fa146d281a8800
karlkallman <137038948+karlkallman@users.noreply.github.com>
2023-07-03 14:54:13 +0200

    Implemented IMCSC function (#933)

---
+++ src/EPPlusTest/FormulaParsing/Excel/Functions/Engineering/ImCscTests.cs
@@ -0,0 +1,132 @@
+﻿using EPPlusTest.ThreadedComments;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EPPlusTest.FormulaParsing.Excel.Functions.Engineering
+{
+    [TestClass]
+    public class ImCscTest
+    {
+        [TestMethod]
+        public void ImCscShouldReturnCorrectResult()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCSC(\"4-3i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("-0.0754898329158637-0.0648774713706355i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImCscShouldReturnCorrectResult2()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCSC(\"4+3i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("-0.0754898329158637+0.0648774713706355i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImCscShouldReturniOnlyResult()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCSC(\"-i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("0.850918128239322i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImCscShouldReturnCorrectZeroIfNullResult()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCSC(\"4-3410.34343453545i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("0", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImCscShouldReturnCorrectWithDecimalInput()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCSC(\"33.34543+8.21312i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("0.00050764041118407+0.000190328339776347i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImCrcShouldReturnCorrectResultOnlyRealNumber()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCSC(\"200\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("-1.1450854172917", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+
+        }
+    }
+}
