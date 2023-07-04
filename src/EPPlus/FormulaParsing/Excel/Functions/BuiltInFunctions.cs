712cf24eb3d7b4dbaa1dadd1e5a7fe21135c5dd2
karlkallman <137038948+karlkallman@users.noreply.github.com>
2023-07-04 09:00:01 +0200

    Implementet IMSIN function (#935)

--- src/EPPlus/FormulaParsing/Excel/Functions/BuiltInFunctions.cs
+++ src/EPPlus/FormulaParsing/Excel/Functions/BuiltInFunctions.cs
@@ -429,6 +429,7 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions
 
             Functions["imcos"] = new ImCos();
             Functions["imcsc"] = new ImCsc();
+            Functions["imsin"] = new ImSin();
         }
     }
 }
