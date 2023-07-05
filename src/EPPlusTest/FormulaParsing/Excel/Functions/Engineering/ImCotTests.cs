f03826505f1c621674449e999a0999887bd7e81d
karlkallman <karl.kallman@epplussoftware.com>
2023-07-05 10:29:06 +0200

    Implemented IMCOT function

---
+++ src/EPPlusTest/FormulaParsing/Excel/Functions/Engineering/ImCotTests.cs
@@ -0,0 +1,227 @@
+﻿using EPPlusTest.ThreadedComments;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EPPlusTest.FormulaParsing.Excel.Functions.Engineering
+{
+    [TestClass]
+    public class ImCotTest
+    {
+        [TestMethod]
+        public void ImCotShouldReturnCorrectResult()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCOT(\"4+3i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("0.00490118239430447-0.999266927805902i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImCotShouldReturniValueOnly()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCOT(\"i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("-1.31303528549933i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImCotShouldReturnRealValueOnly()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCOT(\"2\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("-0.457657554360286", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+
+        [TestMethod]
+        public void ImCotShouldReturniValueOnly2()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCOT(\"0+i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("-1.31303528549933i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImCotShouldReturnCorrectValueifHighImput()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCOT(\"1232+4322i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("-i", result);
+                
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImCotShouldReturnCorrectValueifHighImput2()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCOT(\"12+353i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("-4.42641173779972E-307-i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImCotShouldReturnCorrectResul1()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCOT(\"4+16i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("2.50587932452E-14-0.999999999999996i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImCotShouldReturnCorrectValueifHighImput3()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCOT(\"12+23i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("-1.90725984351442E-20-i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImCotShouldReturnCorrectValueifHighImput4()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCOT(\"12213123+3i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("-0.00479142196748279-1.00128485557364i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImCotShouldReturnNumError()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCOT(\"1221312375564354357+3i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual(ErrorValues.NumError, result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImCotShouldReturniOnly()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMCOT(\"134000000-8578643565436674658i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+    }
+}
