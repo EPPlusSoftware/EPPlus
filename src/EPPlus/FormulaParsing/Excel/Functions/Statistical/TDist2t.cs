d94d3e9525b0db7c0a4f7862e06eff45f65afb60
haalEpplus <137042358+haalEpplus@users.noreply.github.com>
2023-07-05 10:26:44 +0200

    Feature/tdist2t (#939)
    * WIP
    
    * Implemented T.Dist function. Added a Studentthelper class to use for future implementation of formulas using the Students t-distribution. Also added extensive unit-tests for the function
    
    * Added T.DIST.2T and unit-test for the function

---
+++ src/EPPlus/FormulaParsing/Excel/Functions/Statistical/TDist2t.cs
@@ -0,0 +1,38 @@
+﻿using OfficeOpenXml.FormulaParsing.Excel.Functions.Helpers;
+using OfficeOpenXml.FormulaParsing.FormulaExpressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Statistical
+{
+    internal class TDist2t : ExcelFunction
+    {
+        public override int ArgumentMinLength => 2;
+
+        public override CompileResult Execute(IList<FunctionArgument> arguments, ParsingContext context)
+        {
+            var x = ArgToDecimal(arguments, 0);
+            var degreesOfFreedom = ArgToDecimal(arguments, 1);
+
+            //Excel rounds degreesOfFreedom to the nearest int, so we do the same.
+
+            degreesOfFreedom = System.Math.Floor(degreesOfFreedom);
+            if (degreesOfFreedom < 1)
+            {
+                return CreateResult(eErrorType.Num);
+            }
+
+            if (x < 0)
+            {
+                return CreateResult(eErrorType.Num);
+            }
+
+            var result = 2 * (1 - StudenttHelper.CumulativeDistributionFuncion(x, degreesOfFreedom));
+
+            return CreateResult(result, DataType.Decimal);
+            //throw new NotImplementedException();
+        }
+    }
+}
