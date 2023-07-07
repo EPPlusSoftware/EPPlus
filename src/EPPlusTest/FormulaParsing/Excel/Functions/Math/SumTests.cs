e9ddf3a37a8e8d8b1d634582966eff2555c7231a
swmal <{ID}+username}@users.noreply.github.com>
2023-07-07 15:15:10 +0200

    New version of SUM handling difference in bool values via argument and cell reference

--- src/EPPlusTest/FormulaParsing/Excel/Functions/Math/SumTests.cs
+++ src/EPPlusTest/FormulaParsing/Excel/Functions/Math/SumTests.cs
@@ -30,7 +30,7 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.Math
         [TestMethod]
         public void ShouldTreatSingleBooleanValuesAsNumbers()
         {
-            // This test is failing - the logic seems to be
+            // the logic seems to be
             // that boolean values that originates from an evaluation with
             // cell addresses are not counted as numeric values.
             using (var package = new ExcelPackage())
@@ -42,6 +42,77 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.Math
                 sheet.Calculate();
                 var a4val = sheet.Cells["A4"].Value;
                 Assert.AreEqual(0d, a4val);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldCountNumbers()
+        {
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("test");
+                sheet.Cells["A1"].Value = 1;
+                sheet.Cells["A4"].Formula = "SUM(A1,1)";
+                sheet.Calculate();
+                var a4val = sheet.Cells["A4"].Value;
+                Assert.AreEqual(2d, a4val);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldNotCountNumericStrings()
+        {
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("test");
+                sheet.Cells["A1"].Value = "1";
+                sheet.Cells["A4"].Formula = "SUM(A1,\"1\")";
+                sheet.Calculate();
+                var a4val = sheet.Cells["A4"].Value;
+                Assert.AreEqual(0d, a4val);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldCountDates()
+        {
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("test");
+                sheet.Cells["A1"].Value = new DateTime(2023, 7, 7);
+                sheet.Cells["A4"].Formula = "SUM(A1,\"1\")";
+                sheet.Calculate();
+                var a4val = sheet.Cells["A4"].Value;
+                Assert.AreEqual(45114d, a4val);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorFromSingleCellArg()
+        {
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("test");
+                sheet.Cells["A1"].Formula = "1/0";
+                sheet.Cells["A4"].Formula = "SUM(A1)";
+                sheet.Calculate();
+                var a4val = sheet.Cells["A4"].Value;
+                Assert.AreEqual(ExcelErrorValue.Create(eErrorType.Div0), a4val);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorFromMulticellRange()
+        {
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("test");
+                sheet.Cells["A1"].Value = 2;
+                sheet.Cells["A1"].Formula = "1/0";
+                sheet.Cells["A4"].Formula = "SUM(A1:A2)";
+                sheet.Calculate();
+                var a4val = sheet.Cells["A4"].Value;
+                Assert.AreEqual(ExcelErrorValue.Create(eErrorType.Div0), a4val);
             }
         }
     }
