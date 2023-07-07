bb34a22d55220a6e9ed2024f369687a23109e1f8
Adam Shakhabov <Adam.Shakhabov@amana.de>
2023-07-07 14:08:04 +0200

    fix Trim issue

--- src/EPPlusTest/AmanaIssues.cs
+++ src/EPPlusTest/AmanaIssues.cs
@@ -17,9 +17,25 @@ namespace EPPlusTest
 
             // Act-Assert
             package.SaveAs(output);
-            
+
             // Cleanup
             File.Delete(output);
+        }
+
+        [TestMethod]
+        public void ExcelPackage_Calculate()
+        {
+            // Arrange
+            var input = GetTestStream("Trim.xlsx");
+            var package = new ExcelPackage(input);
+            var sheet = package.Workbook.Worksheets[1];
+
+            // Act
+            sheet.Calculate();
+
+            // Arrange
+            Assert.AreEqual("Anlagevermögen", sheet.Cells["B8"].Value);
+            Assert.AreEqual("123 456 ABC", sheet.Cells["B9"].Value);
         }
     }
 }
