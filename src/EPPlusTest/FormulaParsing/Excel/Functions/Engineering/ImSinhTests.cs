7d764e3f583411f045201e989a1bd58563908c0c
karlkallman <karl.kallman@epplussoftware.com>
2023-07-07 10:53:16 +0200

    Implemented IMSINH function

---
+++ src/EPPlusTest/FormulaParsing/Excel/Functions/Engineering/ImSinhTests.cs
@@ -0,0 +1,109 @@
+﻿using EPPlusTest.ThreadedComments;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EPPlusTest.FormulaParsing.Excel.Functions.Engineering
+{
+    [TestClass]
+    public class ImSinhTest
+    {
+        [TestMethod]
+        public void ImSinhShouldReturnCorrectResult()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMSINH(\"4+3i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("-27.0168132580039+3.85373803791938i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImSinhShouldReturnOnlyiResult()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMSINH(\"i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("0.841470984807897i", result);
+
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImSinhShouldReturnCorrectResultHighDecimal()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMSINH(\"12.54623-4.83547i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("17251.325773828+139453.870613546i", result);
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImSinhShouldReturnNumErrorWhenInfinatel()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMSINH(\"12000-50000i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual(ErrorValues.NumError, result);
+            }
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void ImSinhShouldReturnOnlyImaginaryResult()
+        {
+            var ci = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            using (var package = new ExcelPackage())
+
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Formula = "IMSINH(\"0+i\")";
+                sheet.Calculate();
+                var result = sheet.Cells["A1"].Value;
+                Assert.AreEqual("0.841470984807897i", result);
+
+            }
+        }
+    }
+}
