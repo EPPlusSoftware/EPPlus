9faa8ad1484ad3ff12206ea99ac41906c69dc0da
Olga Marchenko <olga.marchenko@amana.de>
2023-07-10 14:49:17 +0200

    #146589: Value from Excel is not mapped correctly in Word

--- src/EPPlusTest/AmanaIssues.cs
+++ src/EPPlusTest/AmanaIssues.cs
@@ -94,5 +94,32 @@ namespace EPPlusTest
             //Assert
             Assert.AreEqual(worksheet.Cells["C2"].Value, worksheet.Cells["E3"].Value);
         }
+
+
+        [TestMethod]
+        public void Test_rounded_values()
+        {
+            //Arrange
+#if Core
+            var dir = AppContext.BaseDirectory;
+            dir = Directory.GetParent(dir).Parent.Parent.Parent.FullName;
+#else
+            var dir = AppDomain.CurrentDomain.BaseDirectory;
+#endif
+            var excelPackage = new ExcelPackage(new FileInfo(Path.Combine(dir, "Workbooks", "TestDoc_WithRoundedValues_xlsx.xlsx")));
+
+            //Act
+            excelPackage.Workbook.Calculate();
+            var table = excelPackage.Workbook.Worksheets[0];
+
+            var value1 = table.Cells["A1"].Value.ToString();
+            var value2 = table.Cells["A4"].Value.ToString();
+            var value3 = table.Cells["B4"].Value.ToString();
+
+            //Asserts
+            Assert.IsTrue(value1.Equals("-18"));
+            Assert.IsTrue(value2.Equals("-40,5"));
+            Assert.IsTrue(value3.Equals("-23,4"));
+        }
     }
 }
