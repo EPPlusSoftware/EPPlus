a36e9d4917361ff14d8616c065212afc1a95aecc
heddaEpplus <137044608+heddaEpplus@users.noreply.github.com>
2023-07-12 15:49:43 +0200

    Implemented imconjugate function (#957)
    Co-authored-by: hedda231 <132577064+hedda231@users.noreply.github.com>
    Co-authored-by: swmal <{ID}+username}@users.noreply.github.com>

--- src/EPPlus/FormulaParsing/Excel/Functions/BuiltInFunctions.cs
+++ src/EPPlus/FormulaParsing/Excel/Functions/BuiltInFunctions.cs
@@ -439,6 +439,7 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions
             Functions["imsinh"] = new ImSinh();
             Functions["imcsch"] = new ImCsch();
             Functions["imcot"] = new ImCot();
+            Functions["imconjugate"] = new ImConjugate();
             Functions["imsech"] = new ImSech();
             Functions["imlog2"] = new ImLog2();
             Functions["gestep"] = new GeStep();
