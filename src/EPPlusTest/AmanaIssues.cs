3d14d9eb7bf945bd96b5b090f28ae378b6bbd45d
Adam Shakhabov <Adam.Shakhabov@amana.de>
2023-07-13 10:07:02 +0200

    adjust tests

--- src/EPPlusTest/AmanaIssues.cs
+++ src/EPPlusTest/AmanaIssues.cs
@@ -316,25 +316,17 @@ namespace EPPlusTest
              "Issue: If a VLookUp-Function contains a Date-Funktion as searchedValue an InvalidCastException is Thrown resulting in an #Value-Result")]
         public void TestIssueWithVLookUpDateValue()
         {
-            //Arrange
-#if Core
-                var dir = AppContext.BaseDirectory;
-                dir = Directory.GetParent(dir).Parent.Parent.Parent.FullName;
-#else
-            var dir = AppDomain.CurrentDomain.BaseDirectory;
-#endif
-            var excelPackage =
-                new ExcelPackage(new FileInfo(Path.Combine(dir, "Workbooks", "TestDoc_VLookUpDateValue_xlsx.xlsx")));
+            // Arrange
+            var xlsx = GetTestStream("TestDoc_VLookUpDateValue_xlsx.xlsx");
+            var package = new ExcelPackage(xlsx);
+            var worksheet = package.Workbook.Worksheets[0];
 
-            //Act
-            var worksheet = excelPackage.Workbook.Worksheets[0];
-
+            // Act
             worksheet.Calculate();
 
             //Assert
             Assert.AreEqual(worksheet.Cells["C2"].Value, worksheet.Cells["E3"].Value);
         }
-
 
         [TestMethod]
         public void Test_rounded_values()
