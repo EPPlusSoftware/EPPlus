113dcad5abc2c5ce1a5a870cb108715cf056b1e4
heddaEpplus <137044608+heddaEpplus@users.noreply.github.com>
2023-07-13 14:00:55 +0200

    Implemented imdiv excel function (#966)
    Co-authored-by: hedda231 <132577064+hedda231@users.noreply.github.com>
    Co-authored-by: swmal <{ID}+username}@users.noreply.github.com>

--- src/EPPlus/FormulaParsing/Excel/Functions/BuiltInFunctions.cs
+++ src/EPPlus/FormulaParsing/Excel/Functions/BuiltInFunctions.cs
@@ -449,6 +449,8 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions
             Functions["imsech"] = new ImSech();
             Functions["imlog2"] = new ImLog2();
             Functions["gestep"] = new GeStep();
+
+            Functions["imdiv"] = new ImDiv();
         }
     }
 }
