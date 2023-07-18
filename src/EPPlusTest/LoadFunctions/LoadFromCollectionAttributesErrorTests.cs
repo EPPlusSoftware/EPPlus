4cf8fd823d487ebe07ef04ee69157cfdd46987b3
Mats Alm <897655+swmal@users.noreply.github.com>
2023-07-18 08:35:24 +0200

    #970 better error message (#971)
    Co-authored-by: swmal <{ID}+username}@users.noreply.github.com>

---
+++ src/EPPlusTest/LoadFunctions/LoadFromCollectionAttributesErrorTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using OfficeOpenXml.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPPlusTest.LoadFunctions
+{
+    public class NestedAttrOnString
+    {
+        [EpplusTableColumn(Order = 1)]
+        public string Name { get; set; }
+
+        [EpplusNestedTableColumn(Order = 2)]
+        public string Value { get; set; }
+    }
+
+    [TestClass]
+    public class LoadFromCollectionAttributesErrorTests
+    {
+        [TestMethod, ExpectedException(typeof(InvalidOperationException))]
+        public void NestedTypeAttributeOnString()
+        {
+            var coll = new List<NestedAttrOnString>
+            {
+                new NestedAttrOnString
+                {
+                    Name = "foo",
+                    Value = "bar",
+                }
+            };
+            using(var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("test");
+                sheet.Cells["A1"].LoadFromCollection(coll);
+            }
+        }
+    }
+}
