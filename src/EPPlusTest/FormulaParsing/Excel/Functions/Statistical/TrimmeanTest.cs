d56806a6b2f86976558e9c8d5f8fd74b201af24b
heddaEpplus <137044608+heddaEpplus@users.noreply.github.com>
2023-07-19 11:33:48 +0200

    Feature/trimmean (#977)
    * working on excel function trimmean
    
    * implemented trimmean function
    
    * added unit test for only one argument
    
    ---------
    
    Co-authored-by: hedda231 <132577064+hedda231@users.noreply.github.com>

--- src/EPPlusTest/FormulaParsing/Excel/Functions/Statistical/TrimmeanTest.cs
+++ src/EPPlusTest/FormulaParsing/Excel/Functions/Statistical/TrimmeanTest.cs
@@ -62,5 +62,18 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.Statistical
                 Assert.AreEqual(ExcelErrorValue.Create(eErrorType.Num), result);
             }
         }
+
+        [TestMethod]
+        public void TrimmeanShouldReturnCorrectResult4()
+        {
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("test");
+                sheet.Cells["A1"].Value = 1;
+                sheet.Cells["A6"].Formula = "TRIMMEAN(A1,20%)";
+                sheet.Calculate();
+                Assert.AreEqual(1d, sheet.Cells["A6"].Value);
+            }
+        }
     }
 }
