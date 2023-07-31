945126132db9f69cf5e99fd46dfa11b5b27ed34f
karlkallman <karl.kallman@epplussoftware.com>
2023-07-31 15:47:07 +0200

    Implemented GAMMA.DIST function. Also added .LowerRegularizedIncompleteGamma method to GammaHelper.

--- src/EPPlus/FormulaParsing/Excel/Functions/BuiltInFunctions.cs
+++ src/EPPlus/FormulaParsing/Excel/Functions/BuiltInFunctions.cs
@@ -202,6 +202,7 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions
             Functions["gamma"] = new Gamma();
             Functions["gammaln"] = new Gammaln();
             Functions["gammaln.precise"] = new GammalnPrecise();
+            Functions["gamma.dist"] = new GammaDotDist();
             Functions["lognorm.inv"] = new LognormDotInv();
             Functions["norminv"] = new NormInv();
             Functions["norm.inv"] = new NormDotInv();
