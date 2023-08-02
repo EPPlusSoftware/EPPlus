2156cb16d4e431af5d6a21e3ad29ed88173766cb
karlkallman <karl.kallman@epplussoftware.com>
2023-08-02 13:36:34 +0200

    WIP BINOM.DIST.RANGE

---
+++ src/EPPlus/FormulaParsing/Excel/Functions/Statistical/BinomDotDistDotRange.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Statistical
+{
+    internal class BinomDotDistDotRange
+    {
+    }
+}
