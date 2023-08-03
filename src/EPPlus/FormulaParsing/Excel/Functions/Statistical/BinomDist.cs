cddc29e4f11d113af1fb49b17dfaf1781420bc80
karlkallman <137038948+karlkallman@users.noreply.github.com>
2023-08-03 09:50:33 +0200

    Added BINOMDIST function and tests (#997)
    * Added BINOMDIST function and tests
    
    * Resolved issues and fixes from the review
    
    * Resolved issues and fixes from the review

---
+++ src/EPPlus/FormulaParsing/Excel/Functions/Statistical/BinomDist.cs
@@ -0,0 +1,66 @@
+﻿/*************************************************************************************************
+  Required Notice: Copyright (C) EPPlus Software AB. 
+  This software is licensed under PolyForm Noncommercial License 1.0.0 
+  and may only be used for noncommercial purposes 
+  https://polyformproject.org/licenses/noncommercial/1.0.0/
+
+  A commercial license to use this software can be purchased at https://epplussoftware.com
+ *************************************************************************************************
+  Date               Author                       Change
+ *************************************************************************************************
+  21/06/2023         EPPlus Software AB       Initial release EPPlus 7
+ *************************************************************************************************/
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Helpers;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Information;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.MathFunctions;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Metadata;
+using OfficeOpenXml.FormulaParsing.FormulaExpressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Statistical
+{
+    [FunctionMetadata(
+    Category = ExcelFunctionCategory.Statistical,
+    EPPlusVersion = "7.0",
+    Description = "Returns the individual term binomial distribution probability.")]
+
+
+    internal class BinomDist : ExcelFunction
+    {
+        public override string NamespacePrefix => "_xlfn.";
+        public override int ArgumentMinLength => 4;
+
+        public override CompileResult Execute(IList<FunctionArgument> arguments, ParsingContext context)
+        {
+            {
+                var numberS = ArgToDecimal(arguments, 0);
+                numberS = Math.Floor(numberS);
+                var trails = ArgToDecimal(arguments, 1);
+                trails = Math.Floor(trails);
+                var probabilityS = ArgToDecimal(arguments, 2);
+                var cumulative = ArgToBool(arguments, 3);
+
+                if (arguments.Count >4) return CompileResult.GetErrorResult(eErrorType.Value);
+                if (numberS < 0 || numberS>trails || probabilityS<0 || probabilityS>1) return CompileResult.GetErrorResult(eErrorType.Num);
+
+                var result = 0d;
+                if (cumulative)
+                {
+                    for (var i = 0; i <= numberS; i++)
+                    {
+                        var combin = MathHelper.Factorial(trails, trails - i) / MathHelper.Factorial(i);
+                        result += combin*Math.Pow(probabilityS, i) * Math.Pow(1 - probabilityS, trails - i);
+                    }
+                }
+                else
+                {
+                    var combin = MathHelper.Factorial(trails, trails - numberS) / MathHelper.Factorial(numberS);
+                    result = combin * Math.Pow(probabilityS, numberS) * Math.Pow(1 - probabilityS, trails - numberS);
+                }
+                return CreateResult(result, DataType.Decimal);
+            }
+        }
+    }
+}
---
+++ src/EPPlusTest/FormulaParsing/Excel/Functions/Statistical/BinomDistTests.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace EPPlusTest.FormulaParsing.Excel.Functions.Statistical
+{
+    [TestClass]
+    public class BinomDistTests
+    {
+
+        [TestMethod]
+        public void BinomDistShouldReturnCorrectResult()
+        {
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("test");
+                sheet.Cells["A2"].Value = 6;
+                sheet.Cells["A3"].Value = 10;
+                sheet.Cells["A4"].Value = 0.5;
+                sheet.Cells["A5"].Value = false;
+
+                sheet.Cells["B5"].Formula = "BINOMDIST(A2,A3,A4,A5)";
+                sheet.Calculate();
+
+                var result = System.Math.Round((double)sheet.Cells["B5"].Value, 7);
+                Assert.AreEqual(0.2050781, result);
+            }
+        }
+
+
+        [TestMethod]
+        public void BinomDistShouldReturnCorrectResultWhenCumulative()
+        {
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("test");
+                sheet.Cells["A2"].Value = 6;
+                sheet.Cells["A3"].Value = 10;
+                sheet.Cells["A4"].Value = 0.5;
+                sheet.Cells["A5"].Value = true;
+
+                sheet.Cells["B5"].Formula = "BINOMDIST(A2,A3,A4,A5)";
+                sheet.Calculate();
+
+                var result = System.Math.Round((double)sheet.Cells["B5"].Value, 7);
+                Assert.AreEqual(0.828125, result);
+            }
+        }
+
+
+        [TestMethod]
+        public void BinomDistShouldReturnCorrectResultWhenCumulative2()
+        {
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("test");
+                sheet.Cells["A2"].Value = 5;
+                sheet.Cells["A3"].Value = 14;
+                sheet.Cells["A4"].Value = 0.7;
+                sheet.Cells["A5"].Value = true;
+
+                sheet.Cells["B5"].Formula = "BINOMDIST(A2,A3,A4,A5)";
+                sheet.Calculate();
+
+                var result = System.Math.Round((double)sheet.Cells["B5"].Value, 7);
+                Assert.AreEqual(0.0082885, result);
+            }
+        }
+
+        [TestMethod]
+        public void BinomDistShouldReturnCorrectResultWhenNotCumulative()
+        {
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("test");
+                sheet.Cells["A2"].Value = 4;
+                sheet.Cells["A3"].Value = 8;
+                sheet.Cells["A4"].Value = 0.3;
+                sheet.Cells["A5"].Value = false;
+
+                sheet.Cells["B5"].Formula = "BINOMDIST(A2,A3,A4,A5)";
+                sheet.Calculate();
+
+                var result = System.Math.Round((double)sheet.Cells["B5"].Value, 7);
+                Assert.AreEqual(0.1361367, result);
+            }
+        }
+
+        [TestMethod]
+        public void BinomDistShouldReturnCorrectResultdecimalInput()
+        {
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("test");
+                sheet.Cells["A2"].Value = 4.432;
+                sheet.Cells["A3"].Value = 8.243534;
+                sheet.Cells["A4"].Value = 0.3;
+                sheet.Cells["A5"].Value = false;
+
+                sheet.Cells["B5"].Formula = "BINOMDIST(A2,A3,A4,A5)";
+                sheet.Calculate();
+
+                var result = System.Math.Round((double)sheet.Cells["B5"].Value, 7);
+                Assert.AreEqual(0.1361367, result);
+            }
+        }
+
+        [TestMethod]
+        public void BinomDistShouldReturnCorrectResultdecimalInput2()
+        {
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("test");
+                sheet.Cells["A2"].Value = 4.832;
+                sheet.Cells["A3"].Value = 8.243534;
+                sheet.Cells["A4"].Value = 0.3;
+                sheet.Cells["A5"].Value = false;
+
+                sheet.Cells["B5"].Formula = "BINOMDIST(A2,A3,A4,A5)";
+                sheet.Calculate();
+
+                var result = System.Math.Round((double)sheet.Cells["B5"].Value, 7);
+                Assert.AreEqual(0.1361367, result);
+            }
+        }
+    }
+}
