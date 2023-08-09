3ae296a8b643d97f22fa0c6a49eb47cc9133303d
karlkallman <karl.kallman@epplussoftware.com>
2023-08-09 13:03:50 +0200

    WIP BINOMINV

---
+++ src/EPPlus/FormulaParsing/Excel/Functions/Statistical/BinomInv.cs
@@ -0,0 +1,51 @@
+﻿/*************************************************************************************************
+  Required Notice: Copyright (C) EPPlus Software AB. 
+  This software is licensed under PolyForm Noncommercial License 1.0.0 
+  and may only be used for noncommercial purposes 
+  https://polyformproject.org/licenses/noncommercial/1.0.0/
+
+  A commercial license to use this software can be purchased at https://epplussoftware.com
+ *************************************************************************************************
+  Date               Author                       Change
+ *************************************************************************************************
+  21/06/2023         EPPlus Software AB       Initial release EPPlus 7
+ *************************************************************************************************/
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Helpers;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Information;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.MathFunctions;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Metadata;
+using OfficeOpenXml.FormulaParsing.FormulaExpressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Statistical
+{
+    [FunctionMetadata(
+    Category = ExcelFunctionCategory.Statistical,
+    EPPlusVersion = "7.0",
+    Description = "Returns the individual term binomial distribution probability.")]
+
+
+    internal class BinomInv : ExcelFunction
+    {
+        public override string NamespacePrefix => "_xlfn.";
+        public override int ArgumentMinLength => 3;
+
+        public override CompileResult Execute(IList<FunctionArgument> arguments, ParsingContext context)
+        {
+            if (arguments.Count > 3) return CompileResult.GetErrorResult(eErrorType.Value);
+
+            var trails = ArgToDecimal(arguments, 0);
+            trails = Math.Floor(trails);
+            var probS = ArgToDecimal(arguments, 1);
+            var alpha = ArgToDecimal(arguments, 2);
+
+            if (trails < 0 || probS <= 0 || probS >= 1 || alpha <= 0 || alpha >= 1) return CompileResult.GetErrorResult(eErrorType.Num);
+
+            var result = 0d;
+
+            return CreateResult(result, DataType.Decimal);
+        }
+    }
+}
