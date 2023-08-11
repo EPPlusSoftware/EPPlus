1a1c963af0fd755d1f12f32f57b08ff4f7867c54
swmal <{ID}+username}@users.noreply.github.com>
2023-08-11 15:04:21 +0200

    #1007 - Fixed handling of ranges in AND and OR functions

--- src/EPPlus/FormulaParsing/Excel/Functions/Logical/And.cs
+++ src/EPPlus/FormulaParsing/Excel/Functions/Logical/And.cs
@@ -16,6 +16,7 @@ using System.Linq;
 using System.Text;
 using OfficeOpenXml.FormulaParsing.Excel.Functions.Metadata;
 using OfficeOpenXml.FormulaParsing.FormulaExpressions;
+using OfficeOpenXml.Utils;
 
 namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Logical
 {
@@ -28,14 +29,73 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Logical
         public override int ArgumentMinLength => 1;
         public override CompileResult Execute(IList<FunctionArgument> arguments, ParsingContext context)
         {
-            for (var x = 0; x < arguments.Count; x++)
+            var argsChecked = 0;
+            for (var x = 0; x < arguments.Count(); x++)
             {
-                var arg = arguments[x];
-                if (arg.DataType == DataType.ExcelError) return new CompileResult(arg.Value, arg.DataType);
-                if (!ArgToBool(arguments, x))
-               {
-                    return new CompileResult(false, DataType.Boolean);
+                var arg = arguments.ElementAt(x);
+                if (arg.IsExcelRange)
+                {
+                    var range = arg.ValueAsRangeInfo;
+                    foreach (var cell in range)
+                    {
+                        var v = cell.Value;
+                        if (v == null)
+                        {
+                            continue;
+                        }
+                        else if (v is ExcelErrorValue)
+                        {
+                            return CreateResult(v, DataType.ExcelError);
+                        }
+                        else if (arg.Address == null && v is string)
+                        {
+                            if (string.IsNullOrEmpty(v.ToString())) continue;
+                            if (!bool.TryParse(v.ToString(), out bool res))
+                            {
+                                argsChecked++;
+                                if (!res) return CreateResult(false, DataType.Boolean);
+                            }
+                        }
+                        else if (!(v is string))
+                        {
+                            var bVal = ConvertUtil.GetValueDouble(v);
+                            argsChecked++;
+                            if (bVal == 0d) return CreateResult(false, DataType.Boolean);
+                        }
+                    }
                 }
+                else
+                {
+                    var singleArg = arguments.ElementAt(x);
+                    var v = singleArg.Value;
+                    if (v == null)
+                    {
+                        continue;
+                    }
+                    else if (v is ExcelErrorValue)
+                    {
+                        return CreateResult(v, DataType.ExcelError);
+                    }
+                    else if (singleArg.Address == null && v is string)
+                    {
+                        if (string.IsNullOrEmpty(v.ToString())) continue;
+                        if (bool.TryParse(v.ToString(), out bool res))
+                        {
+                            argsChecked++;
+                            if (!res) return CreateResult(false, DataType.Boolean);
+                        }
+                    }
+                    else if (!(v is string))
+                    {
+                        var bVal = ConvertUtil.GetValueDouble(v);
+                        argsChecked++;
+                        if (bVal == 0d) return CreateResult(false, DataType.Boolean);
+                    }
+                }
+            }
+            if (argsChecked == 0)
+            {
+                return CreateResult(eErrorType.Value);
             }
             return new CompileResult(true, DataType.Boolean);
         }
