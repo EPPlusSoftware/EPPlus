b46a9d33ebd2aca9b8c24fca3a11e9eebaa0dcb1
Hannes Alm <hannes.alm@epplussoftware.com>
2023-08-15 14:07:25 +0200

    Added license header

--- src/EPPlus/FormulaParsing/Excel/Functions/Finance/Pricemat.cs
+++ src/EPPlus/FormulaParsing/Excel/Functions/Finance/Pricemat.cs
@@ -1,4 +1,17 @@
-﻿using OfficeOpenXml.FormulaParsing.Excel.Functions.Finance.FinancialDayCount;
+﻿/*************************************************************************************************
+  Required Notice: Copyright (C) EPPlus Software AB. 
+  This software is licensed under PolyForm Noncommercial License 1.0.0 
+  and may only be used for noncommercial purposes 
+  https://polyformproject.org/licenses/noncommercial/1.0.0/
+
+  A commercial license to use this software can be purchased at https://epplussoftware.com
+ *************************************************************************************************
+  Date               Author                       Change
+ *************************************************************************************************
+  15/08/2023         EPPlus Software AB           EPPlus v7
+ *************************************************************************************************/
+
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Finance.FinancialDayCount;
 using OfficeOpenXml.FormulaParsing.Excel.Functions.Metadata;
 using OfficeOpenXml.FormulaParsing.FormulaExpressions;
 using System;
@@ -11,7 +24,7 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Finance
     [FunctionMetadata(
     Category = ExcelFunctionCategory.Financial,
     EPPlusVersion = "7.0",
-    Description = ".")]
+    Description = "Calculates the price of a security that pays interest maturity (per 100 dollars)")]
     internal class Pricemat : ExcelFunction
     {
         public override int ArgumentMinLength => 5;
