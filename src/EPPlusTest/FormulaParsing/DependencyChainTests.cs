34910ca853b14dfe4bebaeb2296dbb2bfb138aef
Colby Hearn <colbybhearn@gmail.com>
2023-08-22 09:15:50 -0500

    added tests

--- src/EPPlusTest/FormulaParsing/DependencyChainTests.cs
+++ src/EPPlusTest/FormulaParsing/DependencyChainTests.cs
@@ -14,8 +14,11 @@ namespace EPPlusTest.FormulaParsing
     [TestClass]
     public class DependencyChainTests
     {
+        /// <summary>
+        /// Verifies that the breadth-first chain for this tree is A1 
+        /// </summary>
         [TestMethod]
-        public void NoDepth()
+        public void ChainOrder_OneLink()
         {
             using (var package = new ExcelPackage())
             {
@@ -28,8 +31,11 @@ namespace EPPlusTest.FormulaParsing
             }
         }
 
+        /// <summary>
+        /// Verifies that the breadth-first chain for this tree is A1 > A2 
+        /// </summary>
         [TestMethod]
-        public void LowDepth()
+        public void ChainOrder_TwoLinks()
         {
             using (var package = new ExcelPackage())
             {
@@ -46,18 +52,27 @@ namespace EPPlusTest.FormulaParsing
             }
         }
 
+        /// <summary>
+        /// Verifies that the breadth-first chain for this tree is A1 > A2 > A3
+        /// 
+        ///  A1   depth=0
+        /// |  \
+        /// A2  A3  depth=1
+        /// |   |
+        /// B2  B3  depth=2
+        /// </summary>
         [TestMethod]
-        public void BranchAtDepth0()
+        public void ChainOrder_BranchAtOneDepth()
         {
             using (var package = new ExcelPackage())
             {
-                /*
-                 *   A1   depth=0
-                 *  |  \
-                 *  A2  A3  depth=1
-                 *  |   |
-                 *  B2  B3  depth=2
-                 */
+                ///
+                ///  A1   depth=0
+                /// |  \
+                /// A2  A3  depth=1
+                /// |   |
+                /// B2  B3  depth=2
+                ///
                 var sheet = package.Workbook.Worksheets.Add("test");
                 ExcelRangeBase cellA1 = sheet.Cells["A1"];
                 cellA1.Formula = "A2+A3";
@@ -74,19 +89,20 @@ namespace EPPlusTest.FormulaParsing
             }
         }
 
-
+        /// <summary>
+        /// Verifies that the breadth-first chain for this tree is A1 > A2 > A3
+        /// 
+        ///  A1   depth=0
+        /// |   \
+        /// A2   A3   depth=1
+        /// |  \   \ 
+        /// B2  B3  B4    depth=2
+        /// </summary>
         [TestMethod]
-        public void BranchAtDepth0And1()
+        public void ChainOrder_BranchAtTwoDepths()
         {
             using (var package = new ExcelPackage())
-            {
-                /*
-                 *   A1   depth=0
-                 *  |   \
-                 *  A2   A3   depth=1
-                 *  |  \   \ 
-                 *  B2  B3  B4    depth=2
-                 */
+            {               
                 var sheet = package.Workbook.Worksheets.Add("test");
                 ExcelRangeBase cellA1 = sheet.Cells["A1"];
                 cellA1.Formula = "A2+A3";
@@ -104,21 +120,23 @@ namespace EPPlusTest.FormulaParsing
             }
         }
 
+        /// <summary>
+        /// Verifies that the breadth-first chain for this tree is A1 > B1 > B2 > C1 > C2 > C3
+        /// 
+        ///            A1   depth=0
+        ///            |    
+        ///        B1  +  B2   depth=1
+        ///        |       \     
+        ///    C1  +  C2   C3    depth=2
+        ///    |      |     \
+        ///  D1+D2  D3+D4    D5   depth=3
+        ///
+        /// </summary>
         [TestMethod]
-        public void BranchAtDepths0And1And2()
+        public void ChainOrder_BranchAtThreeDepths()
         {
             using (var package = new ExcelPackage())
-            {
-                /*
-                 *             A1   depth=0
-                 *             |    
-                 *         B1  +  B2   depth=1
-                 *         |       \     
-                 *     C1  +  C2   C3    depth=2
-                 *     |      |     \
-                 *   D1+D2  D3+D4    D5   depth=3
-                 *  
-                 */
+            {                
                 var sheet = package.Workbook.Worksheets.Add("test");
                 ExcelRangeBase cellA1 = sheet.Cells["A1"];
                 cellA1.Formula = "B1+B2";
@@ -144,6 +162,52 @@ namespace EPPlusTest.FormulaParsing
                 Assert.AreEqual(depthTree.ToList()[5].Address, cellC3.Address); // C3 at last
             }
         }
+
+
+        /// <summary>
+        /// This test fails because breadth-first traversal is being simulated in a depth-first algorithm
+        /// Despite A1 referencing B2 "higher" than C1's reference to B2, C1's reference is encountered first, so B2 is treated as being "lower" than C1
+        /// Only truly breadth-first traversal can produce the true breadth-first ordered results
+        /// </summary>
+        [TestMethod]
+        public void ChainOrder_MultipleReferences()
+        {
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("test");
+                ExcelRangeBase A1 = sheet.Cells["A1"];
+                A1.Formula = "B1+B2";  //<--- this instance of a reference B2 will be found second during depth-first tree traversal
+                ExcelRangeBase B1 = sheet.Cells["B1"];
+                B1.Formula = "C1";
+                ExcelRangeBase B2 = sheet.Cells["B2"];
+                B2.Formula = "C2";
+                ExcelRangeBase C1 = sheet.Cells["C1"];
+                C1.Formula = "B2"; //<--- this instance of a reference B2 will be found first during depth-first tree traversal
+
+                IEnumerable<IFormulaCellInfo> depthTree = package.Workbook.FormulaParserManager.GetCalculationChainByDepth(A1);
+                Assert.AreEqual(depthTree.ToList()[0].Address, A1.Address);
+                Assert.AreEqual(depthTree.ToList()[1].Address, B1.Address);
+                Assert.AreEqual(depthTree.ToList()[2].Address, B2.Address); // B2 should come before C1 (but this is a result of simulating breadth-first traversal from a depth-first traversal)
+                Assert.AreEqual(depthTree.ToList()[3].Address, C1.Address); // C1 should be after B2
+            }
+        }
+
+
+        /// <summary>
+        /// This is a template for testing dependency chains from an excel doc
+        /// </summary>
+        [TestMethod]
+        public void ChainOrder_FromFile()
+        {
+            using (var package = new ExcelPackage(@"Dependency Chain Depth.xlsm"))
+            {
+                var sheet = package.Workbook.Worksheets["test"];
+                ExcelRangeBase A1 = sheet.Cells["A1"];
+                IEnumerable<IFormulaCellInfo> depthTree = package.Workbook.FormulaParserManager.GetCalculationChainByDepth(A1);
+            }
+        }
+
+
 
     }
 }
