a7036b1e871cab7fa7547714dbabac48f9af9bf3
OssianEPPlus <122265629+OssianEPPlus@users.noreply.github.com>
2024-01-09 10:44:53 +0100

    Fix/i1238 slow opening7 (#1241)
    * SortedList solution
    
    * Simplified solution without bug
    
    * Moved testcase from issues
    
    * Added fixed issues
    
    * Resolved workbook to worksheet comparison issueResolved workbook to
    worksheet comparison issueResolved workbook to worksheet comparison
    issueResolved workbook to worksheet comparison issueResolved workbook
    to worksheet comparison issueResolved workbook to worksheet comparison
    issueResolved workbook to worksheet comparison issueResolved workbook
    to worksheet comparison issueResolved workbook to worksheet comparison
    issue

---
+++ src/EPPlusTest/Issues/DefinedNameIssues.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EPPlusTest.Issues
+{
+    [TestClass]
+    public class DefinedNameIssues : TestBase
+    {
+        [TestMethod]
+        public void I1238()
+        {
+            using (var p = OpenTemplatePackage("I1238SlowWorkbook.xlsx"))
+            {
+                var ws = p.Workbook.Worksheets[0];
+                ws.Cells["A1"].Value = 1;
+                SaveAndCleanup(p);
+            }
+        }
+    }
+}
