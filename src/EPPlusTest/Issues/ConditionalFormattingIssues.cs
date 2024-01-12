b4a776277e09721abca91d036409fada9795ab3b
OssianEPPlus <122265629+OssianEPPlus@users.noreply.github.com>
2024-01-12 09:14:14 +0100

    Resolved reading and writing issues for databar in some cases i1244 (#1246)
    * Resolved reading and writing issues for databar in some cases
    
    * Fixes

---
+++ src/EPPlusTest/Issues/ConditionalFormattingIssues.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPPlusTest.Issues
+{
+    [TestClass]
+    public class ConditionalFormattingIssues : TestBase
+    {
+        [TestMethod]
+        public void DatabarNegativesAndFormulasTest()
+        {
+            var package = OpenTemplatePackage("i1244Databars.xlsm");
+            Assert.IsNotNull(package.Workbook);
+
+            SaveAndCleanup(package);
+        }
+    }
+}
