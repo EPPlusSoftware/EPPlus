4ebb9d6c0fd3e8946313856a15346b51eb0470e8
JanKallman <jan.kallman@epplussoftware.com>
2024-04-04 09:56:33 +0200

    Fixed issue #1389

--- src/EPPlusTest/Issues/ChartIssues.cs
+++ src/EPPlusTest/Issues/ChartIssues.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OfficeOpenXml;
 using OfficeOpenXml.Drawing;
 using OfficeOpenXml.Drawing.Chart;
 namespace EPPlusTest.Issues
