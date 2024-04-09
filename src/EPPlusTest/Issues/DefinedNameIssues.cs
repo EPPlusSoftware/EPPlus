33f947f624800f9ffcca293c1c84f54b7f0283de
Ossian Edström <ossian.edstrom@epplussoftware.com>
2024-04-09 16:34:31 +0200

    Resolving #1408

--- src/EPPlusTest/Issues/DefinedNameIssues.cs
+++ src/EPPlusTest/Issues/DefinedNameIssues.cs
@@ -2,6 +2,7 @@
 using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace EPPlusTest.Issues
@@ -30,5 +31,54 @@ namespace EPPlusTest.Issues
                 SaveWorkbook("s652.xlsx", p2);
             }
 		}
+        [TestMethod]
+        //i1408
+        public void VersionName()
+        {
+            using (var package = OpenTemplatePackage("VersionNameManager.xlsx"))
+            {
+                var name = package.Workbook.Names.First();
+
+                SaveAndCleanup(package);
+            }
+        }
+
+        [TestMethod]
+        //i1408
+        public void DefinedNamesQuoteError()
+        {
+            using (var package = OpenPackage("QuoteError.xlsx", true))
+            {
+                package.Workbook.Worksheets.Add("something");
+                package.Workbook.Names.AddValue("Lae_Zel", "zhak vo\"n\"fynh duj");
+
+                var packageTemp = OpenPackage("dummyQuoteWorkbook.xlsx", true);
+                packageTemp.Workbook.Worksheets.Add("dummy");
+                SaveAndCleanup (packageTemp);
+
+                var file = new FileInfo("C:\\epplusTest\\Testoutput\\dummyQuoteWorkbook.xlsx");
+
+                package.Workbook.ExternalLinks.AddExternalWorkbook(file);
+
+                package.Workbook.Names.AddFormula("编制单位", "\"编制单位：\"&[1]dummyQuoteWorkbook!$D$6");
+
+
+                //Adding "s\"\"omething" here correctly? results in corrupt worksheet.
+                package.Workbook.Names.AddValue("Unended", "s\"omething");
+
+
+                SaveAndCleanup(package);
+            }
+
+            using (var package = OpenPackage("QuoteError.xlsx"))
+            {
+
+                Assert.AreEqual("zhak vo\"n\"fynh duj", package.Workbook.Names["Lae_Zel"].Value);
+                Assert.AreEqual("\"编制单位：\"&[1]dummyQuoteWorkbook!$D$6", package.Workbook.Names["编制单位"].Formula);
+                Assert.AreEqual("s\"omething", package.Workbook.Names["Unended"].Value);
+
+                SaveAndCleanup(package);
+            }
+        }
     }
 }
