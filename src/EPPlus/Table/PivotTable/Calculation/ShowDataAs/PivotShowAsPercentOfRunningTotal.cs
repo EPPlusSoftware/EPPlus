85403ea8abf45c101737f65e6ef7bb1d8571d854
Ossian Edström <ossian.edstrom@epplussoftware.com>
2024-05-14 16:26:59 +0200

    Further tests

--- src/EPPlus/Table/PivotTable/Calculation/ShowDataAs/PivotShowAsPercentOfRunningTotal.cs
+++ src/EPPlus/Table/PivotTable/Calculation/ShowDataAs/PivotShowAsPercentOfRunningTotal.cs
@@ -10,59 +10,43 @@ Date               Author                       Change
 *************************************************************************************************
 01/18/2024         EPPlus Software AB       EPPlus 7.2
 *************************************************************************************************/
-using OfficeOpenXml.ConditionalFormatting;
-using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 
 namespace OfficeOpenXml.Table.PivotTable.Calculation.ShowDataAs
 {
-	internal class PivotShowAsPercentOfRunningTotal : PivotShowAsRunningTotal
+    internal class PivotShowAsPercentOfRunningTotal : PivotShowAsRunningTotal
 	{
 		internal override void Calculate(ExcelPivotTableDataField df, List<int> fieldIndex, ref PivotCalculationStore calculatedItems)
 		{
-			var bf = fieldIndex.IndexOf(df.BaseField);
-			var colFieldsStart = df.Field.PivotTable.RowFields.Count;
-			var keyCol = fieldIndex.IndexOf(df.BaseField);
-			var record = df.Field.PivotTable.CacheDefinition._cacheReference.Records;
-			var maxBfKey = 0;
-
 			CalculateRunningTotal(df, fieldIndex, ref calculatedItems, true);
-
-			if (record.CacheItems[df.BaseField].Count(x => x is int) > 0)
-			{
-				maxBfKey = (int)record.CacheItems[df.BaseField].Where(x => x is int).Max();
-			}
-
-			foreach (var key in calculatedItems.Index.OrderBy(x => x.Key, ArrayComparer.Instance))
-			{
-				if (IsSumBefore(key.Key, bf, fieldIndex, colFieldsStart))
-				{
-					calculatedItems[key.Key] = null;
-				}
-				else
-				{
-					if (IsSumAfter(key.Key, bf, fieldIndex, colFieldsStart))
-					{
-						var parentKey = GetParentKey(key.Key, keyCol);
-						var parentValue = calculatedItems[parentKey];
-						if (parentValue is double pv)
-						{
-							calculatedItems[key.Key] = (double)calculatedItems[key.Key] / pv;
-						}
-						else if (calculatedItems[key.Key] is not ExcelErrorValue)
-						{
-							calculatedItems[key.Key] = parentValue;
-						}
-					}
-					else
-					{
-						calculatedItems[key.Key] = 1D;
-					}
-				}
-			}
-		}
-	}
+            foreach (var key in calculatedItems.Index.OrderBy(x => x.Key, ArrayComparer.Instance))
+            {
+                if (IsSumBefore(key.Key, _bf, fieldIndex, _colFieldsStart))
+                {
+                    calculatedItems[key.Key] = null;
+                }
+                else
+                {
+                    if (IsSumAfter(key.Key, _bf, fieldIndex, _colFieldsStart))
+                    {
+                        var parentKey = GetParentKey(key.Key, _keyCol);
+                        var parentValue = calculatedItems[parentKey];
+                        if (parentValue is double pv)
+                        {
+                            calculatedItems[key.Key] = (double)calculatedItems[key.Key] / pv;
+                        }
+                        else if (calculatedItems[key.Key] is not ExcelErrorValue)
+                        {
+                            calculatedItems[key.Key] = parentValue;
+                        }
+                    }
+                    else
+                    {
+                        calculatedItems[key.Key] = 1D;
+                    }
+                }
+            }
+        }
+    }
 }
--- src/EPPlus/Table/PivotTable/Calculation/ShowDataAs/PivotShowAsRunningTotal.cs
+++ src/EPPlus/Table/PivotTable/Calculation/ShowDataAs/PivotShowAsRunningTotal.cs
@@ -10,74 +10,89 @@
 *************************************************************************************************
  01/18/2024         EPPlus Software AB       EPPlus 7.2
 *************************************************************************************************/
-using System;
+using EPPlusTest.Table.PivotTable;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 
 namespace OfficeOpenXml.Table.PivotTable.Calculation.ShowDataAs
 {
     internal class PivotShowAsRunningTotal : PivotShowAsBase
     {
+		protected static int _bf;
+        protected static int _colFieldsStart;
+        protected static int _keyCol;
+        protected static PivotTableCacheRecords _record;
+        protected static int _maxBfKey;
+
         internal override void Calculate(ExcelPivotTableDataField df, List<int> fieldIndex, ref PivotCalculationStore calculatedItems)
 		{
 			CalculateRunningTotal(df, fieldIndex, ref calculatedItems, false);
 		}
 
-		internal static void CalculateRunningTotal(ExcelPivotTableDataField df, List<int> fieldIndex, ref PivotCalculationStore calculatedItems, bool leaveParentSum)
+		internal void CalculateRunningTotal(ExcelPivotTableDataField df, List<int> fieldIndex, ref PivotCalculationStore calculatedItems, bool leaveParentSum)
 		{
-			var bf = fieldIndex.IndexOf(df.BaseField);
-			var colFieldsStart = df.Field.PivotTable.RowFields.Count;
-			var keyCol = fieldIndex.IndexOf(df.BaseField);
-			var record = df.Field.PivotTable.CacheDefinition._cacheReference.Records;
-			var maxBfKey = 0;
-			if (record.CacheItems[df.BaseField].Count(x => x is int) > 0)
+			_bf = fieldIndex.IndexOf(df.BaseField);
+
+            if (_bf < 0)
+            {
+                foreach (var key in calculatedItems.Index.OrderBy(x => x.Key, ArrayComparer.Instance))
+                {
+                    calculatedItems[key.Key] = ErrorValues.NAError;
+                }
+				return;
+            }
+
+            _colFieldsStart = df.Field.PivotTable.RowFields.Count;
+			_keyCol = fieldIndex.IndexOf(df.BaseField);
+			_record = df.Field.PivotTable.CacheDefinition._cacheReference.Records;
+			_maxBfKey = 0;
+
+			if (_record.CacheItems[df.BaseField].Count(x => x is int) > 0)
 			{
-				maxBfKey = (int)record.CacheItems[df.BaseField].Where(x => x is int).Max();
+				_maxBfKey = (int)_record.CacheItems[df.BaseField].Where(x => x is int).Max();
 			}
 
-			foreach (var key in calculatedItems.Index.OrderBy(x => x.Key, ArrayComparer.Instance))
-			{
-				if (IsSumBefore(key.Key, bf, fieldIndex, colFieldsStart))
-				{
-					if(!(leaveParentSum == true && key.Key[keyCol] == PivotCalculationStore.SumLevelValue))
-					{
-						calculatedItems[key.Key] = null;
-					}
-				}
-				else if (IsSumAfter(key.Key, bf, fieldIndex, colFieldsStart) == true)
-				{
-					if (key.Key[keyCol] > 0)
-					{
-						var prevKey = GetPrevKey(key.Key, keyCol);
-						if (calculatedItems.ContainsKey(prevKey))
-						{
-							if (calculatedItems[key.Key] is double current)
-							{
-								if (calculatedItems[prevKey] is double prev)
-								{
-									calculatedItems[key.Key] = current + prev;
-								}
-								else
-								{
-									calculatedItems[key.Key] = calculatedItems[prevKey]; //The prev key is an error, set the value to that error.
-								}
-							}
-						}
-					}
+            foreach (var key in calculatedItems.Index.OrderBy(x => x.Key, ArrayComparer.Instance))
+            {
+                if (IsSumBefore(key.Key, _bf, fieldIndex, _colFieldsStart))
+                {
+                    if (!(leaveParentSum == true && key.Key[_keyCol] == PivotCalculationStore.SumLevelValue))
+                    {
+                        calculatedItems[key.Key] = null;
+                    }
+                }
+                else if (IsSumAfter(key.Key, _bf, fieldIndex, _colFieldsStart) == true)
+                {
+                    if (key.Key[_keyCol] > 0)
+                    {
+                        var prevKey = GetPrevKey(key.Key, _keyCol);
+                        if (calculatedItems.ContainsKey(prevKey))
+                        {
+                            if (calculatedItems[key.Key] is double current)
+                            {
+                                if (calculatedItems[prevKey] is double prev)
+                                {
+                                    calculatedItems[key.Key] = current + prev;
+                                }
+                                else
+                                {
+                                    calculatedItems[key.Key] = calculatedItems[prevKey]; //The prev key is an error, set the value to that error.
+                                }
+                            }
+                        }
+                    }
 
-					if (key.Key[keyCol] < maxBfKey)
-					{
-						var nextKey = GetNextKey(key.Key, keyCol);
-						while (nextKey[keyCol] < maxBfKey && calculatedItems.ContainsKey(nextKey) == false)
-						{
-							calculatedItems[nextKey] = calculatedItems[key.Key];
-							nextKey = GetNextKey(nextKey, keyCol);
-						}
-					}
-				}
-			}
-		}
+                    if (key.Key[_keyCol] < _maxBfKey)
+                    {
+                        var nextKey = GetNextKey(key.Key, _keyCol);
+                        while (nextKey[_keyCol] < _maxBfKey && calculatedItems.ContainsKey(nextKey) == false)
+                        {
+                            calculatedItems[nextKey] = calculatedItems[key.Key];
+                            nextKey = GetNextKey(nextKey, _keyCol);
+                        }
+                    }
+                }
+            }
+        }
 	}
 }
