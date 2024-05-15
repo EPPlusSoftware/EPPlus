e41ab9cf544def3dfd385c47ffb8d666bd2ae60f
OssianEPPlus <122265629+OssianEPPlus@users.noreply.github.com>
2024-05-15 09:34:17 +0200

    Fixed formulas for non-formula types (#1447)
    * Fixed formulas for non-formula types
    
    * corrected read-in parse

--- src/EPPlus/ConditionalFormatting/ExcelConditionalFormattingIconSetBase.cs
+++ src/EPPlus/ConditionalFormatting/ExcelConditionalFormattingIconSetBase.cs
@@ -15,6 +15,8 @@ using System;
 using System.Globalization;
 using System.Xml;
 using OfficeOpenXml.ConditionalFormatting.Contracts;
+using OfficeOpenXml.FormulaParsing.Utilities;
+using OfficeOpenXml.Utils;
 using OfficeOpenXml.Utils.Extensions;
 
 namespace OfficeOpenXml.ConditionalFormatting
@@ -158,7 +160,16 @@ namespace OfficeOpenXml.ConditionalFormatting
             icon.Type = xr.GetAttribute("type").ToEnum<eExcelConditionalFormattingValueObjectType>().Value;
             if (icon.Type != eExcelConditionalFormattingValueObjectType.Formula)
             {
-                icon.Value = double.Parse(xr.GetAttribute("val"), CultureInfo.InvariantCulture);
+                var iconValue = xr.GetAttribute("val");
+
+                if(ConvertUtil.TryParseNumericString(iconValue, out double numValue))
+                {
+                    icon.Value = numValue;
+                }
+                else
+                {
+                    icon.Formula = iconValue;
+                }
             }
             else
             {
