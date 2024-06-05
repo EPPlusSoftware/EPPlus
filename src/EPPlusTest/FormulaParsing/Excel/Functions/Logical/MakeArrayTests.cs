1f6a6e935163bcd0eaf6e5587fc363a539ea34f2
swmal <897655+swmal@users.noreply.github.com>
2024-06-05 16:56:29 +0200

    Added test for MakeArray

--- src/EPPlusTest/FormulaParsing/Excel/Functions/Logical/MakeArrayTests.cs
+++ src/EPPlusTest/FormulaParsing/Excel/Functions/Logical/MakeArrayTests.cs
@@ -22,5 +22,29 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.Logical
             Assert.AreEqual(3d, sheet.Cells["B1"].Value);
             Assert.AreEqual(4d, sheet.Cells["B2"].Value);
         }
+
+        [TestMethod]
+        public void MakeArray_Test2()
+        {
+            using var package = new ExcelPackage();
+            var sheet = package.Workbook.Worksheets.Add("Sheet1");
+            sheet.Cells["D4"].Formula = "MAKEARRAY(D2,E2,LAMBDA(row,col,CHOOSE(RANDBETWEEN(1,3),\"Red\",\"Blue\",\"Green\")))";
+            sheet.Cells["D2"].Value = 10;
+            sheet.Cells["E2"].Value = 1;
+            sheet.Calculate();
+            bool IsValidCellValue(object val)
+            {
+                var arr = new string[] { "Red", "Blue", "Green" };
+                foreach (var str in arr)
+                {
+                    if (string.Compare(str, val.ToString()) == 0) return true;
+                }
+                return false;
+            };
+            var d4 = sheet.Cells["D4"].Value;
+            var d5 = sheet.Cells["D5"].Value;
+            Assert.IsTrue(IsValidCellValue(d4));
+            Assert.IsTrue(!IsValidCellValue(d5));
+        }
     }
 }
