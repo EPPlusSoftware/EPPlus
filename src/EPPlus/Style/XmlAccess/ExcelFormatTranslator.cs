57ccad5f11853e72811167b1a65e98d634216dae
Maximilian Kodat <maximilianko@amana.lucanet.com>
2024-06-11 08:58:37 +0200

    REX-848: Missed line

--- src/EPPlus/Style/XmlAccess/ExcelFormatTranslator.cs
+++ src/EPPlus/Style/XmlAccess/ExcelFormatTranslator.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using static OfficeOpenXml.Style.XmlAccess.ExcelNumberFormatXml;
 
 namespace OfficeOpenXml.Style.XmlAccess
