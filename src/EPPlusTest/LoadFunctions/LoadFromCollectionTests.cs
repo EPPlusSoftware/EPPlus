b8e114314445a8b5a1cbb54841ae2cb88c7e1503
Ossian Edström <ossian.edstrom@epplussoftware.com>
2024-07-11 10:36:38 +0200

    Fixed bug

--- src/EPPlusTest/LoadFunctions/LoadFromCollectionTests.cs
+++ src/EPPlusTest/LoadFunctions/LoadFromCollectionTests.cs
@@ -126,6 +126,15 @@ namespace EPPlusTest.LoadFunctions
             public string CamelCased_And_Underscored { get; set; }
         }
 
+        [EpplusTable(ShowTotal = true)]
+        public class BaseBillingReportExportModel
+        {
+            [EpplusTableColumn(Order = 0, TotalsRowLabel = "Totals", NumberFormat = "m/d/yyyy")]
+            public DateTime RegistrationDate { get; set; }
+            [EpplusTableColumn(Order = 1)]
+            public string RegistrantName { get; set; }
+        }
+
         internal class UrlClass : BClass
         {
             [EpplusIgnore]
@@ -488,6 +497,26 @@ namespace EPPlusTest.LoadFunctions
                     c.HeaderParsingType = HeaderParsingTypes.CamelCaseToSpace;
                 });
                 Assert.AreEqual("Id Of This Instance", sheet.Cells["C1"].Value);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldParseCamelCasedHeadersWhenColumned()
+        {
+            var items = new List<BaseBillingReportExportModel>()
+            {
+                new BaseBillingReportExportModel(){ RegistrantName = "someName" }
+            };
+            using (var pck = new ExcelPackage(new MemoryStream()))
+            {
+                var sheet = pck.Workbook.Worksheets.Add("sheet");
+                sheet.Cells["C1"].LoadFromCollection(items, c =>
+                {
+                    c.PrintHeaders = true;
+                    c.HeaderParsingType = HeaderParsingTypes.CamelCaseToSpace;
+                });
+                Assert.AreEqual("Registration Date", sheet.Cells["C1"].Value);
+                Assert.AreEqual("Registrant Name", sheet.Cells["D1"].Value);
             }
         }
 
