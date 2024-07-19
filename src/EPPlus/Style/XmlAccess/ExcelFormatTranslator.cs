0f7f230c92a98ab90f29adc0acf1dca75445d8e9
JanKallman <jan.kallman@epplussoftware.com>
2024-07-19 08:59:18 +0200

    Removed using

--- src/EPPlus/Style/XmlAccess/ExcelFormatTranslator.cs
+++ src/EPPlus/Style/XmlAccess/ExcelFormatTranslator.cs
@@ -5,7 +5,6 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading;
 using static OfficeOpenXml.Style.XmlAccess.ExcelNumberFormatXml;
 
 namespace OfficeOpenXml.Style.XmlAccess
