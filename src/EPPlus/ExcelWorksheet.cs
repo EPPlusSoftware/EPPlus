36abd11e9693c63dd932397a7fdfa1b63b14402f
JanKallman <jan.kallman@epplussoftware.com>
2024-08-08 13:07:36 +0200

    Fixes issue #1556

--- src/EPPlus/ExcelWorksheet.cs
+++ src/EPPlus/ExcelWorksheet.cs
@@ -1220,6 +1220,7 @@ namespace OfficeOpenXml
                 e = zip.GetNextEntry();
             }
             while (e.FileDataPosition != entry.FileDataPosition);
+            zip.Position = entry.FileDataPosition;
         }
 
         /// <summary>
