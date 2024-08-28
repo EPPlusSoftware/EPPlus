8c8bc8e67dfb86d6ed713e83942a8e547a671b05
JanKallman <jan.kallman@epplussoftware.com>
2024-08-28 08:33:48 +0200

    Added test file

---
+++ src/EPPlusTest/EncryptionTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+
+namespace EPPlusTest
+{
+    [TestClass]
+    public class EncryptionTests : TestBase
+    {
+        [TestMethod]
+        public void SensitivityLableRead()
+        {
+            var fi = GetTemplateFile("SensitivityLabel.xlsx");
+            using(var p=new ExcelPackage(fi,""))
+            {
+                Assert.AreEqual(1, p.Workbook.Worksheets.Count);
+            }
+        }
+    }
+}
