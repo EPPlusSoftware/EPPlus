51c2bc76510e890ca95a868dd0ad38bce79e6dc6
Ossian Edström <ossian.edstrom@epplussoftware.com>
2024-11-26 09:52:17 +0100

    Reverted change that caused invalid signatures

--- src/EPPlus/Packaging/ZipPackage.cs
+++ src/EPPlus/Packaging/ZipPackage.cs
@@ -349,17 +349,8 @@ namespace OfficeOpenXml.Packaging
                             partStream.Write(bArr, 0, bArr.Length);
                             partStream.Seek(0, SeekOrigin.Begin);
                             
-                            //Remove unncesesary spaces on end-nodes like Excel
-                            var xmlStr = Encoding.UTF8.GetString(bArr);
-                            xmlStr = xmlStr.Replace(" />", "/>");
-                            var xmlBytes = Encoding.UTF8.GetBytes(xmlStr);
-                            
-                            using(var ms = new MemoryStream(xmlBytes))
-                            {
-                                ms.Seek(0, SeekOrigin.Begin);
-                                var part = GetPart(uri);
-                                Manifest.AddPartToManifest(part, ms);
-                            }
+                            var part = GetPart(uri);
+                            Manifest.AddPartToManifest(part, partStream);
                         }
                     }
                 }
