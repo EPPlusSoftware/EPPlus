51a8a9d514bdd846eedc590b5d81065ba045bdd8
AdrianParnéus <adrian.parneus@epplussoftware.com>
2024-12-13 16:37:19 +0100

    fixed broken linking #1759

--- src/EPPlusTest/Drawing/OLETests.cs
+++ src/EPPlusTest/Drawing/OLETests.cs
@@ -578,5 +578,20 @@ namespace EPPlusTest.Drawing
                 SaveAndCleanup(genericOlePackage);
             }
         }
+
+
+        [TestMethod]
+        public void OleNotworkingTest()
+        {
+            //Write Generic Object
+            using var genericOlePackage = OpenPackage("EpplusOleObject_Embed_Generic.xlsx", true);
+            var generiWs = genericOlePackage.Workbook.Worksheets.Add("Sheet 1");
+            var myFile = Properties.Resources.GetOLEObjectFullFileName("MyTextDocument.txt");
+            var genericOle = generiWs.Drawings.AddOleObject("MyTextFile", myFile);
+            Assert.IsTrue(genericOle._document.Storage.DataStreams.ContainsKey(Ole10Native.OLE10NATIVE_STREAM_NAME));
+            Assert.IsTrue(genericOle._document.Storage.DataStreams.ContainsKey(CompObj.COMPOBJ_STREAM_NAME));
+            Assert.IsFalse(genericOle.IsExternalLink);
+            SaveAndCleanup(genericOlePackage);
+        }
     }
 }
