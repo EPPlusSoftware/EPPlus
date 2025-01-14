0c9aeb45a038052020d02347c7e6176205a5a334
Ossian Edström <ossian.edstrom@epplussoftware.com>
2025-01-14 14:08:37 +0100

    Cleaned up unnecesary method

--- src/EPPlus/Core/Worksheet/WorksheetRangeHelper.cs
+++ src/EPPlus/Core/Worksheet/WorksheetRangeHelper.cs
@@ -194,11 +194,6 @@ namespace OfficeOpenXml.Core.Worksheet
             deletedDrawings.ForEach(d => ws.Drawings.Remove(d));
         }
 
-        internal static void AdjustCommentsRow()
-        {
-
-        }
-
         internal static void AdjustDrawingsColumn(ExcelWorksheet ws, int columnFrom, int columns, int rowFrom = 0, int rowTo = ExcelPackage.MaxRows)
         {
             var deletedDrawings = new List<ExcelDrawing>();
