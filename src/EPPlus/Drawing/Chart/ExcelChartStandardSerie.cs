1b501736785018b5b122d5e965b25a4b8fd220f2
OssianEPPlus <122265629+OssianEPPlus@users.noreply.github.com>
2025-03-13 13:13:38 +0100

    Fixed double parsing (#1899)

--- src/EPPlus/Drawing/Chart/ExcelChartStandardSerie.cs
+++ src/EPPlus/Drawing/Chart/ExcelChartStandardSerie.cs
@@ -308,8 +308,11 @@ namespace OfficeOpenXml.Drawing.Chart
             {
                 if(node.LocalName == "pt")
                 {
-                    var txt = node.InnerText;
-                    numLits.Add(Convert.ToDouble(txt));
+                    if(double.TryParse(node.InnerText, NumberStyles.Any, CultureInfo.InvariantCulture, out double numLit) == false)
+                    {
+                        throw new InvalidDataException($"numberLiteral in xml node:'{node.Name}' in chart:'{_chart.Name}' with value:'{node.InnerText}' could not be parsed as double. Chart cannot be read.");
+                    }
+                    numLits.Add(numLit);
                 }
             }
             numberLiterals = numLits.ToArray();
