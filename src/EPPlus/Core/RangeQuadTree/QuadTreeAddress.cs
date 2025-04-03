63e634f68e40f65a13b2c01fea73237d1aeeca83
JanKallman <jan.kallman@epplussoftware.com>
2025-04-03 13:29:44 +0200

    Removed commented out code

--- src/EPPlus/Core/RangeQuadTree/QuadTreeAddress.cs
+++ src/EPPlus/Core/RangeQuadTree/QuadTreeAddress.cs
@@ -29,43 +29,6 @@ namespace OfficeOpenXml.Core.RangeQuadTree
             {
                 l.Add(rangeItem.CloneWithNewAddress(clearedRange.ToRow+1, Math.Max(r.FromCol, clearedRange.FromCol), r.ToRow, Math.Min(r.ToCol, clearedRange.ToCol)));
             }
-            //if (clearedRange.FromRow >= r.FromRow)
-            //{
-            //    if (clearedRange.FromCol > r.FromCol)
-            //    {
-            //        l.Add(rangeItem.CloneWithNewAddress(r.FromRow, r.FromCol, r.ToRow, clearedRange.FromCol-1));
-            //    }
-            //    if (clearedRange.ToCol < r.ToCol)
-            //    {
-            //        l.Add(rangeItem.CloneWithNewAddress(r.FromRow, clearedRange.FromCol+1, r.ToRow, r.ToCol));
-            //    }
-            //}
-            //else
-            //{
-            //    int fromRow;
-            //    if (clearedRange.FromCol <= r.FromCol && clearedRange.ToCol < r.ToCol)
-            //    {
-            //        l.Add(rangeItem.CloneWithNewAddress(r.FromRow, r.FromCol, clearedRange.FromRow - 1, clearedRange.ToCol));
-            //        fromRow = clearedRange.FromRow - 1;
-            //    }
-            //    else
-            //    {
-            //        fromRow=r.FromRow;
-            //    }
-            //    if (clearedRange.FromCol > r.FromCol)
-            //    {
-            //        l.Add(rangeItem.CloneWithNewAddress(fromRow, r.FromCol, r.ToRow, clearedRange.FromCol - 1));
-            //    }
-            //    if (clearedRange.ToCol < r.ToCol)
-            //    {
-            //        l.Add(rangeItem.CloneWithNewAddress(r.FromRow, clearedRange.ToCol + 1, r.ToRow, r.ToCol));
-            //    }
-            //}
-
-            //if(clearedRange.ToRow < r.ToRow)
-            //{
-            //    l.Add(rangeItem.CloneWithNewAddress(clearedRange.ToRow + 1, Math.Max(clearedRange.FromCol, r.FromCol), r.ToRow, Math.Min(clearedRange.ToCol, r.ToCol)));
-            //}
             return l;
         }
     }
