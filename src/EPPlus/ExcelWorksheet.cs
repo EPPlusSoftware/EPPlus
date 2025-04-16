ca25a1f3a8c6b080fc0af4973479a94f46bd6b00
Jan Källman <jan.kallman@epplussoftware.com>
2025-04-16 09:33:15 +0200

    Fixes issue 1957 and 1958 (#1959)
    * Fixes issue #1957 and #1958
    
    * Fix for dynamic array formula update
    
    * Renamed ChooseFunction

--- src/EPPlus/ExcelWorksheet.cs
+++ src/EPPlus/ExcelWorksheet.cs
@@ -974,10 +974,10 @@ namespace OfficeOpenXml
                 if (col == "")
                 {
                     double tint = GetXmlNodeDouble(tabColorPath + "/@tint");
-                    eThemeSchemeColor theme = (eThemeSchemeColor)GetXmlNodeInt(tabColorPath + "/@theme");
+                    eThemeSchemeColor? theme = (eThemeSchemeColor?)GetXmlNodeIntNull(tabColorPath + "/@theme");
                     int indexed = GetXmlNodeInt(tabColorPath + "/@indexed");
                     bool auto = GetXmlNodeBool(tabColorPath + "/@auto");
-                    if (tint == double.NaN)
+                    if (double.IsNaN(tint))
                     {
                         return Color.Empty;
                     }
@@ -3199,7 +3199,7 @@ namespace OfficeOpenXml
         }
         internal ExcelPivotTableCollection _pivotTables = null;
         /// <summary>
-        /// Pivottables defined in the worksheet.
+        /// Pivot tables defined in the worksheet.
         /// </summary>
         public ExcelPivotTableCollection PivotTables
         {
