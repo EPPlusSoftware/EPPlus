e0e35c31f8a1813e4b579f1131ed1f0d79670e78
AdrianEPPlus <162118292+AdrianEPPlus@users.noreply.github.com>
2025-04-23 16:59:28 +0200

    Fixed freezing pane not taking hidden rows and columns into considera… (#1969)
    * Fixed freezing pane not taking hidden rows and columns into consideration.
    
    * made fast

--- src/EPPlus/ExternalReferences/ExcelExternalWorkbook.cs
+++ src/EPPlus/ExternalReferences/ExcelExternalWorkbook.cs
@@ -682,7 +682,7 @@ namespace OfficeOpenXml.ExternalReferences
                         }
                     }
                 }
-            }            
+            }
         }
 
         /// <summary>
