bf86b24573e3eee469d6be165a51775601ca77f6
Olivier Renaud <ore@advanthink.com>
2025-07-03 17:42:44 +0200

    Remove a call to Type.GetProperty to improve AOT support
    EPPlus is mostly usable in a native AOT compilation context. One exception to
    this support is the use of the reflection method Type.GetProperty in
    TypeCompat.GetPropertyValue.
    
    Fortunately, there is only one place where this method is used, in
    ExcelVBACollectionBase. By adding a type constraint on the generic type T of
    ExcelVBACollectionBase, we can get rid of the use of reflection.
    
    In practice, an AOT compiled project that uses EPPlus to read an Excel file
    that contains VBA macro could throw NullReferenceExceptions.

---
+++ src/EPPlus/Vba/IExcelVBACollectionElement.cs
@@ -0,0 +1,9 @@
+namespace OfficeOpenXml.VBA {
+
+    /// <summary>
+    /// The interface that must be implemented by the elements stored by ExcelVBACollectionBase.
+    /// </summary>
+    public interface IExcelVBACollectionElement {
+        string Name{get;}
+    }
+}
