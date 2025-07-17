678e19e4166e53454f2631551234231635d57e75
Mats Alm <897655+swmal@users.noreply.github.com>
2025-07-17 19:10:21 +0200

    #2054 - changed to InvariantCulture when writing CF icons to the xml (#2055)
    * #2054 - changed to InvariantCulture when writing ConditionalFormatting icons to the xml
    
    * Fixed culture settings in unitests
    
    * Fixed Thread.CurrentThread.CurrentCulture issue in test project
    
    ---------
    
    Co-authored-by: swmal <{ID}+username}@users.noreply.github.com>

--- src/EPPlusTest/Issues/ConditionalFormattingIssues.cs
+++ src/EPPlusTest/Issues/ConditionalFormattingIssues.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OfficeOpenXml;
+using OfficeOpenXml.ConditionalFormatting;
 using OfficeOpenXml.Style;
+using System.Globalization;
+using System.Threading;
 
 namespace EPPlusTest.Issues
 {
@@ -77,6 +80,48 @@ namespace EPPlusTest.Issues
 
                 SaveAndCleanup(package);
             }
+        }
+
+        [TestMethod]
+        public void Test1_Input_ExpectedOutput()
+        {
+            var currentCulture = Thread.CurrentThread.CurrentCulture;
+            // if this is InvariantCulture, everything works fine:
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("de-DE");
+
+            using var package = OpenPackage("i2054.xlsx", delete: true);
+
+            var worksheet = package.Workbook.Worksheets.Add("test");
+            int fromRow = 1;
+            int toRow = 10;
+            var range = worksheet.Cells[fromRow, 1, toRow, 1];
+
+            for (int i = fromRow; i <= toRow; i++)
+            {
+                worksheet.Cells[i, 1].Value = i;
+            }
+
+            var iconSet =
+                range.ConditionalFormatting.AddThreeIconSet(
+                    eExcelconditionalFormatting3IconsSetType.Symbols2);
+
+            // icons are counted bottom up, when compared to Excel UI, so Icon3 is the topmost one:
+            iconSet.Icon1.Type = eExcelConditionalFormattingValueObjectType.Num;
+            iconSet.Icon1.Value = 0;
+
+            iconSet.Icon2.Type = eExcelConditionalFormattingValueObjectType.Num;
+            iconSet.Icon2.Value = 1.5; // this is the problem: get's written to the XML as 1,5
+
+            iconSet.Icon3.Type = eExcelConditionalFormattingValueObjectType.Num;
+            iconSet.Icon3.Value = 3;
+            iconSet.Icon3.GreaterThanOrEqualTo = false;
+
+            iconSet.Icon1.CustomIcon = eExcelconditionalFormattingCustomIcon.BlackCircle;
+            iconSet.Icon2.CustomIcon = eExcelconditionalFormattingCustomIcon.BlackCircle;
+            iconSet.Icon3.CustomIcon = eExcelconditionalFormattingCustomIcon.GoldStar;
+
+            SaveAndCleanup(package);
+            Thread.CurrentThread.CurrentCulture = currentCulture;
         }
     }
 }
