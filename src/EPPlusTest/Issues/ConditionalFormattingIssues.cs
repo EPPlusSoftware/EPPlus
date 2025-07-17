bffd5dcd46a538c95d0b6c352c72cdf4fb9e46f9
swmal <{ID}+username}@users.noreply.github.com>
2025-07-17 13:42:36 +0200

    Fixed issue with CurrentCulture in unit tests

--- src/EPPlusTest/Issues/ConditionalFormattingIssues.cs
+++ src/EPPlusTest/Issues/ConditionalFormattingIssues.cs
@@ -85,7 +85,7 @@ namespace EPPlusTest.Issues
         [TestMethod]
         public void Test1_Input_ExpectedOutput()
         {
-
+            var currentCulture = Thread.CurrentThread.CurrentCulture;
             // if this is InvariantCulture, everything works fine:
             Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("de-DE");
 
@@ -121,6 +121,8 @@ namespace EPPlusTest.Issues
             iconSet.Icon3.CustomIcon = eExcelconditionalFormattingCustomIcon.GoldStar;
 
             SaveAndCleanup(package);
+
+            Thread.CurrentThread.CurrentCulture = currentCulture;
         }
     }
 }
