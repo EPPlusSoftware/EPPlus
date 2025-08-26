6e5771558dfa481ea61b63d4c8340fa098a423a2
JanKallman <jan.kallman@epplussoftware.com>
2025-08-26 15:38:26 +0200

    Added ColorExtenstions

---
+++ src/EPPlus/Utils/ColorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace OfficeOpenXml.Utils
+{
+    internal static class ColorExtentions
+    {
+        internal static string To6CharHexString(this Color color)
+        {
+            return (color.ToArgb() & 0xFFFFFF).ToString("x6");
+        }
+    }
+
+}
