34fa17feed1810bc776e6fe66d3821c617610e7b
Ossian Edström <ossian.edstrom@epplussoftware.com>
2025-09-08 15:29:00 +0200

    Assembly level run in parallel- 12 tests fail

--- src/EPPlusTest/Properties/AssemblyInfo.cs
+++ src/EPPlusTest/Properties/AssemblyInfo.cs
@@ -42,7 +42,9 @@
 //[assembly: AssemblyCopyright("Copyright © Jan Källman")]
 //[assembly: AssemblyTrademark("")]
 //[assembly: AssemblyCulture("")]
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+[assembly: Parallelize]
 //// Setting ComVisible to false makes the types in this assembly not visible 
 //// to COM componenets.  If you need to access a type in this assembly from 
 //// COM, set the ComVisible attribute to true on that type.
