d62b12787e66c1254ddfaf750667598838d58785
karlkallman <karl.kallman@epplussoftware.com>
2025-09-23 10:05:50 +0200

    Changed Worksheet.View.Firstsheet access modifier to public. #2114

---
+++ src/EPPlusTest/Core/WorkbookViewTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using System;
+
+namespace EPPlusTest.Core
+{
+    [TestClass]
+    public class WorkbookViewTest : TestBase
+    {
+
+        [TestMethod]
+        public void ValidateFirstSheet()
+        {
+            using(var p = OpenPackage("firstSheet.xlsx", true))
+            {
+                for (var i= 0; i <14; i++)
+                {
+                    p.Workbook.Worksheets.Add(i.ToString());
+                }
+                p.Workbook.View.FirstSheet = 5;
+
+                Assert.AreEqual(5, p.Workbook.View.FirstSheet);
+                SaveAndCleanup(p);
+            }
+        }        
+    }
+}
