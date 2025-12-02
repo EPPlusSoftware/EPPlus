5c351e8e3930bd7462799416064729b2633a3bb6
Jan Källman <jan.kallman@epplussoftware.com>
2025-12-02 13:29:44 +0100

    Removed commented-out code.

--- src/EPPlus/Utils/TextUtils.cs
+++ src/EPPlus/Utils/TextUtils.cs
@@ -5,24 +5,6 @@ namespace OfficeOpenXml.Utils
 {
     internal static class TextUtils
     {
-        //internal static double PointToPixel(this double pointSize)
-        //{
-        //    //1 inch is 72 pts. "Inches * dots/inch = dots" aka Pixels
-        //    return pointSize / 72 * 96;
-        //}
-
-        //internal static float PointToPixel(this float pointSize)
-        //{
-        //    //1 inch is 72 pts. "Inches * dots/inch = dots" aka Pixels
-        //    return pointSize / 72 * 96;
-        //}
-
-        //internal static double PixelToPoint(this double pixelSize)
-        //{
-        //    var roundedSize = (double)RoundToWhole(pixelSize);
-        //    return roundedSize / 96 * 72;
-        //}
-
         internal static double EmuToPoint(this double emuNumber)
         {
             return emuNumber / ExcelDrawing.EMU_PER_POINT;
