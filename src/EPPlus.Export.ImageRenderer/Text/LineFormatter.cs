d6b7add0df2bb4b9c999bde02b1361bc3ceb1996
OssianEPPlus <122265629+OssianEPPlus@users.noreply.github.com>
2025-12-03 13:20:00 +0100

    BugFix #2201 and start of RichText support for textwrap in shapes (#2202)
    * Fixed scaling the textbox to text size for svg rendering
    
    * Added locks to remove racing multi-threading issues
    
    * Starting multi-line sizing operations
    
    * added fix for #2201 (preserveWhitSpace)
    
    * Start of line-handling§

---
+++ src/EPPlus.Export.ImageRenderer/Text/LineFormatter.cs
@@ -0,0 +1,27 @@
+﻿using OfficeOpenXml.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EPPlus.Export.ImageRenderer.Text
+{
+    internal static class LineFormatter
+    {
+        internal static /*List<TextLine>*/ void GetFormattedLines(ExcelTextBody body)
+        {
+            var paragraphs = body.Paragraphs;
+            //
+            var text = paragraphs.Text;
+            for (int i = 0; i < paragraphs.Count; i++) 
+            {
+                var p = paragraphs[i];
+                for(int j = 0; j < p.TextRuns.Count; j++)
+                {
+                    var run = p.TextRuns[j];
+                    run.SplitIntoLines();
+                }
+            }
+        }
+    }
+}
