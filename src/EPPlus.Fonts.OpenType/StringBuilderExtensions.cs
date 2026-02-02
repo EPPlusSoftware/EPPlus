0f6f7a111dba0b72a45ed1805d9258f7c90847cf
Ossian Edström <ossian.edstrom@epplussoftware.com>
2026-02-02 13:29:32 +0100

    Added failing test

---
+++ src/EPPlus.Fonts.OpenType/StringBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EPPlus.Fonts.OpenType
+{
+    internal static class StringBuilderExtensions
+    {
+        /// <summary>
+        /// If the last char in the stringbuilder is a space
+        /// trim it by adjusting length
+        /// </summary>
+        /// <param name="sb"></param>
+        public static void TrimEndSpace(this StringBuilder sb)
+        {
+            if (sb.Length > 0 && sb[sb.Length - 1] == ' ')
+            {
+                sb.Length--;
+            }
+        }
+        /// <summary>
+        /// If there is anything in the stringbuilder append space
+        /// </summary>
+        /// <param name="sb"></param>
+        public static void AppendSpace(this StringBuilder sb)
+        {
+            if (sb.Length > 0)
+            {
+                sb.Append(' ');
+            }
+        }
+    }
+}
