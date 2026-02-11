a2f5fbbf0e3b39fec2d1bfa829793a24d869e93a
Ossian Edström <ossian.edstrom@epplussoftware.com>
2026-02-11 12:55:19 +0100

    fixed bug in textData. Started textLines refactor

---
+++ src/EPPlus.Fonts.OpenType/Integration/LineFragment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EPPlus.Fonts.OpenType.Integration
+{
+    /// <summary>
+    /// The fragment of a richTextFragment that is within a line
+    /// </summary>
+    public class LineFragment
+    {
+        public int StartIdx { get; set; }
+        public double Width { get; set; }
+        public int RtIdx { get; set; }
+
+        internal LineFragment(int rtFragmentIdx, int idxWithinLine)
+        {
+            RtIdx = rtFragmentIdx;
+            StartIdx = idxWithinLine;
+        }
+    }
+}
