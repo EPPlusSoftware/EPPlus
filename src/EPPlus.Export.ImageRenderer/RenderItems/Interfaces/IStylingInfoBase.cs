3b9f6f4843849e1182c254d488a7250aeb11f94a
Ossian Edström <ossian.edstrom@epplussoftware.com>
2026-03-24 17:33:16 +0100

    storage commit for proof-of-concept ideas

---
+++ src/EPPlus.Export.ImageRenderer/RenderItems/Interfaces/IStylingInfoBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EPPlus.Export.ImageRenderer.RenderItems.Interfaces
+{
+    internal interface IStylingInfoBase
+    {
+
+    }
+}
