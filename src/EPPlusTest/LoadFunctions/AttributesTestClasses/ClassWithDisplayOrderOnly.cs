fd93cea6dce3dd1ce8c7510da803cdcce9df596c
swmal <897655+swmal@users.noreply.github.com>
2026-04-14 16:07:15 +0200

    #2331, #2332 - fix for combining EpplusTableColumn and Display attributes in LoadFromCollection.

---
+++ src/EPPlusTest/LoadFunctions/AttributesTestClasses/ClassWithDisplayOrderOnly.cs
@@ -0,0 +1,29 @@
+﻿/*************************************************************************************************
+  Required Notice: Copyright (C) EPPlus Software AB. 
+  This software is licensed under PolyForm Noncommercial License 1.0.0 
+  and may only be used for noncommercial purposes 
+  https://polyformproject.org/licenses/noncommercial/1.0.0/
+  A commercial license to use this software can be purchased at https://epplussoftware.com
+ *************************************************************************************************/
+#if !NET35
+using System.ComponentModel.DataAnnotations;
+
+namespace EPPlusTest.LoadFunctions.AttributesTestClasses
+{
+    /// <summary>
+    /// Test class where only DisplayAttribute is present (no EpplusTableColumnAttribute).
+    /// DisplayAttribute.Order should be used in this case.
+    /// </summary>
+    public class ClassWithDisplayOrderOnly
+    {
+        [Display(Name = "Id Column", Order = 3)]
+        public int Id { get; set; }
+
+        [Display(Name = "Name Column", Order = 1)]
+        public string Name { get; set; }
+
+        [Display(Name = "Description Column", Order = 2)]
+        public string Description { get; set; }
+    }
+}
+#endif
