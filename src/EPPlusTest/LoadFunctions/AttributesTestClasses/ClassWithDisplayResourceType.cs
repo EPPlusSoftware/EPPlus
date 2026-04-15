3995501f75caaa2e243474cedf885409ab125b2e
Mats Alm <897655+swmal@users.noreply.github.com>
2026-04-15 08:41:07 +0200

    #2331, #2332 - fix for combining EpplusTableColumn and Display attributes in LoadFromCollection. (#2333)

---
+++ src/EPPlusTest/LoadFunctions/AttributesTestClasses/ClassWithDisplayResourceType.cs
@@ -0,0 +1,35 @@
+﻿/*************************************************************************************************
+  Required Notice: Copyright (C) EPPlus Software AB. 
+  This software is licensed under PolyForm Noncommercial License 1.0.0 
+  and may only be used for noncommercial purposes 
+  https://polyformproject.org/licenses/noncommercial/1.0.0/
+
+  A commercial license to use this software can be purchased at https://epplussoftware.com
+ *************************************************************************************************/
+using OfficeOpenXml.Attributes;
+#if !NET35
+using System.ComponentModel.DataAnnotations;
+
+namespace EPPlusTest.LoadFunctions.AttributesTestClasses
+{
+    /// <summary>
+    /// Test class using DisplayAttribute with ResourceType.
+    /// When ResourceType is set, GetName() should be used instead of Name
+    /// to get the localized value from the resource class.
+    /// </summary>
+    public class ClassWithDisplayResourceType
+    {
+        [EpplusTableColumn(Order = 1)]
+        [Display(Name = "IdHeader", ResourceType = typeof(LoadFromCollResources))]
+        public int Id { get; set; }
+
+        [EpplusTableColumn(Order = 2)]
+        [Display(Name = "NameHeader", ResourceType = typeof(LoadFromCollResources))]
+        public string Name { get; set; }
+
+        [EpplusTableColumn(Order = 3)]
+        [Display(Name = "DescriptionHeader", ResourceType = typeof(LoadFromCollResources))]
+        public string Description { get; set; }
+    }
+}
+#endif
