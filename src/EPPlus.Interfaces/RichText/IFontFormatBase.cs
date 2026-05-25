b578b3695a6ebc0fbfea9c992db14d61bddc0acc
Ossian Edström <ossian.edstrom@epplussoftware.com>
2026-05-25 15:10:17 +0200

    Added export to openTypeFormat for richText

---
+++ src/EPPlus.Interfaces/RichText/IFontFormatBase.cs
@@ -0,0 +1,21 @@
+﻿using OfficeOpenXml.Interfaces.Fonts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfficeOpenXml.Interfaces.RichText
+{
+    public interface IFontFormatBase
+    {
+        string Family { get; set; }
+        FontSubFamily SubFamily { get; set; }
+        float Size { get; set; }
+
+
+        /// <summary>
+        /// Sets the underlying data properties to be equal to the font
+        /// </summary>
+        /// <param name="font"></param>
+        public void SetFont(IFontFormatBase font);
+    }
+}
