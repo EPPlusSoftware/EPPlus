91c78adef5288c68bfebb231006d5bf9c834b7dd
swmal <897655+swmal@users.noreply.github.com>
2026-05-26 14:14:13 +0200

    Added test for legacy arrayformulas

--- src/EPPlusTest/FormulaParsing/Excel/Functions/InformationFunctionsTests.cs
+++ src/EPPlusTest/FormulaParsing/Excel/Functions/InformationFunctionsTests.cs
@@ -528,6 +528,29 @@ namespace EPPlusTest.Excel.Functions
         }
 
         [TestMethod]
+        public void IsFormulaShouldReturnCorrectValueWhenLegacyArrayFormula()
+        {
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                sheet.Cells["A1"].Value = 1;
+                sheet.Cells["A2"].Value = 3;
+                sheet.Cells["A3"].Value = 5;
+                sheet.Cells["B1"].Value = 2;
+                sheet.Cells["B2"].Value = 4;
+                sheet.Cells["B3"].Value = 6;
+                sheet.Cells["E6"].CreateArrayFormula("A1:B3+1");
+                sheet.Cells["E11"].Formula = "ISFORMULA(E6)";
+                sheet.Cells["F11"].Formula = "ISFORMULA(F6)";
+                sheet.Calculate();
+                var result = sheet.Cells["E11"].Value;
+                Assert.IsTrue((bool)result);
+                result = sheet.Cells["F11"].Value;
+                Assert.IsFalse((bool)result);
+            }
+        }
+
+        [TestMethod]
         public void IsFormulaShouldReturnCorrectValueWhenDynamicArrayFormula()
         {
             using (var package = new ExcelPackage())
