92483265fdd9ad02ab34e44be81149d02ef8724c
swmal <897655+swmal@users.noreply.github.com>
2026-06-10 14:34:48 +0200

    Added more unittests with improvment areas

--- src/EPPlus.Export.Pdf.Tests/PdfTests.cs
+++ src/EPPlus.Export.Pdf.Tests/PdfTests.cs
@@ -219,7 +219,8 @@ namespace EPPlusTest.PDF
         //// 3. Row headings are sligthly wider in EPPlus than in Excel.
         //[DataRow("Aico_0105_S_ALR_87011990_AICO_ASSET_ITE_2025-04_BS.xlsx", "C:\\epplustest\\pdf\\OutputTest1.2.pdf", "Summary")]
         //[DataRow("Aico WiP 120180 FBL3N for 0110 in 2025-04.xlsx", "C:\\epplustest\\pdf\\OutputTest1.4.pdf", "Technical")]
-        [DataRow("Aico KKS1 Variance Calculation for 0105 in 2025-04 (25_4_2025 15_43_40) .xlsx", "C:\\epplustest\\pdf\\OutputTest1.5.pdf", "Technical")]
+        //[DataRow("Aico KKS1 Variance Calculation for 0105 in 2025-04 (25_4_2025 15_43_40) .xlsx", "C:\\epplustest\\pdf\\OutputTest1.5.pdf", "Technical")]
+        [DataRow("R05.xlsx", "C:\\epplustest\\pdf\\OutputTest1.6.pdf", "R05 Arbeitseinteilung")]
         public void WorkbookTests(string sourceFile, string outputPath, string wsName)
         {
             using var p = OpenTemplatePackage(sourceFile);
