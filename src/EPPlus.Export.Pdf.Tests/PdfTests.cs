f4c6bf508b4cd9b8a3bb40ab620fc81b718ad416
AdrianParnéus <adrian.parneus@epplussoftware.com>
2026-06-25 15:19:33 +0200

    Updated todo.

--- src/EPPlus.Export.Pdf.Tests/PdfTests.cs
+++ src/EPPlus.Export.Pdf.Tests/PdfTests.cs
@@ -27,84 +27,33 @@ namespace EPPlusTest.PDF
     public class PdfTests : TestBase
     {
         /* BIG PDF TODO
-         * This should be turned into tickets on github..
          * 
-         * FEATURES:
-         * Cells: Icons
-         * Cells: Pictures in cells : Prio 3
-         * Cells: Conditional Formatting
-         * Cells: Calculate clip rect for merged cells with content bounds in mind
+         * Missing Features:
+         * Embedding pictures as drawings
+         * Embedding pictures as cell content
+         * Embedding pictures as header/footer
+         * Number formatting
+         * Scaling document
+         * Conditional Formatting icons
+         * Vertical text
+         * Equations
+         * Pivot tables
+         * Shapes
+         * Charts
+         * 3D models
+         * Compression
          * 
-         * Text: Wrap text
-         * Text: Vertical text again
-         * Text: Equations : Prio 5
-         * Text: Shrink To Fit (Scaling)
+         * Bugs:
+         * Merged cell uses full width/height even when columns/rows are hidden.
+         * Conditional formatting not worksing 100% of the time.
+         * Table could sometimes select wrong style
          * 
-         * Layout: Scaling
-         * Layout: Fit to number of pages (Scaling)
-         * Layout: Excel Workbook to pdf (Export)
-         * Layout: Selected worksheets to pdf (Export)
-         * Layout: Cell range to pdf (Export)
-         * Layout: Print Area (This is like a collection of cell ranges) (Export)
-         * Layout: Print titles
-         * Layout: Background (Excel does not add this to pdf it seems) (Images) : Prio 4
-         * Layout: Remove empty last page
-         * Layout: Autofit row
-         * 
-         * Pivot Table: Pivot table implementation : Prio 5
-         * 
-         * Drawings: Pictures (Images) :  Prio 3
-         * Drawings Shapes (Images) : Prio 5
-         * Drawings: Charts (Images) : Prio 5
-         * Drawings: 3D models (Images) : Prio 5
-         * 
-         * PDF: Compress parts
-         * 
-         * IMPROVEMENTS;
+         * Other
          * Cells: Remove stroke from solid fill and adjust size and position of cell more precise and only use fill command.
-         * 
-         * Tables: Table Implementation
-         * Tables: Fix order of applying table styling. Right now Table is used if it exsists, but cell styling should be prioritised and table should be ignored if cell has styluing.
-         * 
-         * Patterns: Adjust and make patterns look better. Fixed: DarkUp, DarkDown
-         * 
          * Gradients: Make diamond gradients instead of radial gradtient in from corner and center gradient
-         * 
          * Text: Set up to use back up techniques when not embedding font
-         * 
-         * Layout: Calculate height of cells more correctly
-         *
          * Borders: Adjust and make border look better
-         */
-
-        /* REFACTOR:
-         * 1. Collect: Gather all text in all worksheets, headerfooter and comments
-         * 2. Shape  : Shape the Text in all worksheets, headerfooter and comments
-         * 3. Layout : Autofit row, add print titles, add row and column headings, set up print areas, page breaks
-         * 4. Pages: : Create number of pages needed, assign elements to pages
-         * 5. PDF    : Export to pdf
          * 
-         * 
-         * --------------0. Set up ---------------------
-         * Create class catalog that takes in a workbook, or a collection of worksheets, or a range.
-         * When step1 is done we check in a comments collections if there are comments and process the comments worksheet the same way.
-         * for each worksheet do following:
-         * --------------1. Collect---------------------
-         * Create array map of worksheet with coords, text, fills and so on.
-         * This is done when all worksheets have been mapped.
-         * Comments and notes will create a temporary worksheet in current workbook with all notes and comments inside cells.
-         * --------------2. Shape  ---------------------
-         * Go through all text in all workbbooks and shape the text.
-         * We also check text height measurements and adjust row height.
-         * --------------3. Layout ---------------------
-         * Precalculate content area of each page using content area and page breaks and print areas. We need to take scaling into account here!
-         * Add pictures, shapes, notes and other elements that lies on top of cells.
-         * Then loop the collection and insert row and column headings, print titles.
-         * We also need to create gridlines here
-         * --------------4. Pages ----------------------
-         * Create Pages and create transform objects of each element from the array and assign them to their respective page.
-         * --------------5. PDF ------------------------
-         * When all worksheets have been processed, combine all into one catalog and create the pdf document.
          */
 
         /// <summary>
