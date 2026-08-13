def1e2311e8eb663fe254febb5231047f3821dff
AdrianParnéus <adrian.parneus@epplussoftware.com>
2026-08-13 16:59:48 +0200

    added new project to break out DotNetZip

---
+++ src/EPPlus.Export.Pdf/Helpers/PdfFlate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPPlus.Export.Pdf.Helpers
+{
+    internal static class PdfFlate
+    {
+        internal static byte[] Compress(byte[] data)
+        {
+            //using (var ms = new MemoryStream())
+            //{
+            //    using (var zs = new ZlibStream(ms, CompressionMode.Compress, CompressionLevel.BestCompression))
+            //    {
+            //        zs.Write(data, 0, data.Length);
+            //    }
+            //    return ms.ToArray();
+            //}
+            return null;
+        }
+    }
+}
