5f6ea8f72e41c09ab9b6ad1740fd28c7db026a19
karlkallman <137038948+karlkallman@users.noreply.github.com>
2026-09-10 08:06:33 +0200

    Bug/1069 (#2508)
    * Fixed bug surrounding FullPrecision being lost when saving in EPPlus
    
    * Cleanup

--- src/EPPlus/ExcelWorkbook.cs
+++ src/EPPlus/ExcelWorkbook.cs
@@ -1556,7 +1556,7 @@ namespace OfficeOpenXml
 
             DeleteCalcChain();
 
-            SetXmlNodeBool("d:calcPr/@fullPrecision", FullPrecision, false);
+            SetXmlNodeBool("d:calcPr/@fullPrecision", FullPrecision, true);
             
             if(_workbookCreatedInEPPlus == false) EnsureCalculationFeatures();    //Ensure that the calculation features are included in the file to make sure that new functions are supported.
 
