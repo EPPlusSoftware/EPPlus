d3260be3fea1c797f6b218f42e923f054b85f419
Mats Alm <897655+swmal@users.noreply.github.com>
2026-09-14 15:25:08 +0200

    Color3 was null (#2520)
    * Added tests for LookupColor/PDF issue
    
    * Fixed color3
    
    ---------
    
    Co-authored-by: AdrianParnéus <adrian.parneus@epplussoftware.com>

--- src/EPPlus.Export.Pdf.Tests/PdfTests.cs
+++ src/EPPlus.Export.Pdf.Tests/PdfTests.cs
@@ -13,6 +13,7 @@
 using EPPlus.Export.Pdf.Settings;
 using EPPlus.Export.Pdf.Settings.PdfPageSizes;
 using EPPlus.Export.Pdf.Tests;
+using EPPlus.Export.Pdf.Tests.Helpers;
 using OfficeOpenXml;
 using OfficeOpenXml.Export.PdfExport;
 using OfficeOpenXml.Export.PdfExport.Data;
@@ -530,6 +531,21 @@ namespace EPPlusTest.PDF
             ws.Cells["C1"].Value = "C1";
 
             SaveAsPdf(ws, "ThreeFonts_NoSkip.pdf");
+        }
+
+        [TestMethod]
+        public void PdfColorIssue_WithCopiedWorksheet()
+        {
+            var helper = new ColorIssueTestHelper();
+            using var package = helper.CreateWorkbook();
+            SaveAsPdf(package.Workbook, "Allsvenskan2001.pdf");
+        }
+
+        [TestMethod]
+        public void PdfColorIssue2_WithSingleWorkbook()
+        {
+            using var package = OpenTemplatePackage("Allsvenskan2001.xlsx");
+            SaveAsPdf(package.Workbook, "Allsvenskan2001.pdf");
         }
 
         [TestMethod]
